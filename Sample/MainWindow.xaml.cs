72edf2f3aa7112edaf08c07702453f75d0236110
Samuel Carlsson <samuel.carlsson@gmail.com>
2014-09-27 11:17:56 +0200

    Adding sample project

---
+++ Sample/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using notf;
+using System.IO;
+using System.Windows;
+
+namespace Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var f = new FileInfo(@"C:\Users\vidstige\Desktop\segoe\segoeui.ttf");
+            var reader = new OpenTypeReader();
+            using (var stream = f.OpenRead())
+            {
+                reader.Reader(stream);
+            }
+        }
+    }
+}
---
+++ notf/OpenTypeReader.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace notf
+{
+    public class OpenTypeReader
+    {
+        public void Reader(Stream stream)
+        {
+            using (var input = new BinaryReader(stream))
+            {
+                
+            }
+        }
+    }
+}
