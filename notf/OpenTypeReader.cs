6c9c3f591e2fc1c8dac625b3f8ac65a6cc3a8e08
Samuel Carlsson <samuel.carlsson@gmail.com>
2014-09-27 12:06:50 +0200

    Refactor: class for table entries

--- notf/OpenTypeReader.cs
+++ notf/OpenTypeReader.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
@@ -9,7 +11,7 @@ namespace notf
         public void Reader(Stream stream)
         {
             var little = BitConverter.IsLittleEndian;
-            using (var input = new ByteOrderSwappingBinaryReader(stream))
+            using (BinaryReader input = new ByteOrderSwappingBinaryReader(stream))
             {
                 UInt32 version = input.ReadUInt32();
                 UInt16 tableCount = input.ReadUInt16();
@@ -17,23 +19,49 @@ namespace notf
                 UInt16 entrySelector = input.ReadUInt16();
                 UInt16 rangeShift = input.ReadUInt16();
 
+                var tables = new List<TableEntry>(tableCount);
                 for (int i = 0; i < tableCount; i++)
                 {
-                    var tag = input.ReadUInt32();
-                    var checkSum = input.ReadUInt32();
-                    var offset = input.ReadUInt32();
-                    var length = input.ReadUInt32();
-
-                    Console.WriteLine(TagToString(tag));
+                    tables.Add(Table.ReadFrom(input));
                 }
+
+                var cmap = tables.Single(t => t.Tag == "glyf");
+
             }
         }
 
-        private String TagToString(uint tag)
+        private class TableEntry
         {
-            var bytes = BitConverter.GetBytes(tag);
-            Array.Reverse(bytes);
-            return Encoding.ASCII.GetString(bytes);
+            private readonly uint _tag;
+            private readonly uint _checkSum;
+            private readonly uint _offset;
+            private readonly uint _length;
+
+            public string Tag { get { return TagToString(_tag); } }
+            private TableEntry(uint tag, uint checkSum, uint offset, uint length)
+            {
+                _tag = tag;
+                _checkSum = checkSum;
+                _offset = offset;
+                _length = length;
+
+            }
+
+            public static TableEntry ReadFrom(BinaryReader input)
+            {
+                var tag = input.ReadUInt32();
+                var checkSum = input.ReadUInt32();
+                var offset = input.ReadUInt32();
+                var length = input.ReadUInt32();
+                return new Table(tag, checkSum, offset, length);
+            }
+            private String TagToString(uint tag)
+            {
+                var bytes = BitConverter.GetBytes(tag);
+                Array.Reverse(bytes);
+                return Encoding.ASCII.GetString(bytes);
+            }
         }
+
     }
 }
