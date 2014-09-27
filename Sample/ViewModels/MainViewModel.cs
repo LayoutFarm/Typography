5cc79de334a957ba8f4366b7a274c08d4a318ad7
Samuel Carlsson <samuel.carlsson@gmail.com>
2014-09-27 18:33:57 +0200

    Displays glyph points

--- Sample/ViewModels/MainViewModel.cs
+++ Sample/ViewModels/MainViewModel.cs
@@ -1,21 +1,60 @@
 ﻿using notf;
+using notf.Tables;
 using Sample.ViewModels.Commands;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 
 namespace Sample.ViewModels
 {
-    public class MainViewModel
+    public class MainViewModel: ViewModel
     {
         private Typeface _typeface;
 
         private List<Point> _points = new List<Point>();
+        private int _selected;
 
         public List<Point> Points
         {
             get { return _points; }
+            private set
+            {
+                _points = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public Typeface Typeface
+        {
+            get { return _typeface; }
+            private set
+            {
+                _typeface = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private IEnumerable<Point> ToPoints(Glyph glyph)
+        {
+            var x = glyph.X;
+            var y = glyph.Y;
+            for (int i = 0; i < glyph.PointCount; i++)
+                yield return new Point(x[i], y[i]);
+        }
+
+        public int SelectedGlyph
+        {
+            get { return _selected; }
+            set
+            {
+                _selected = value;
+                RaisePropertyChanged();
+
+                // Update points
+                Points = ToPoints(_typeface.Glyphs[_selected]).ToList();
+            }
         }
 
         public ICommand Load { get { return new DelegatingCommand(LoadTypeface); } }
@@ -31,7 +70,7 @@ namespace Sample.ViewModels
         private void LoadTypeface()
         {
             var f = new FileInfo(@"C:\Users\vidstige\Desktop\segoe\segoeui.ttf");
-            _typeface = LoadFrom(f);
+            Typeface = LoadFrom(f);
         }
     }
 }
