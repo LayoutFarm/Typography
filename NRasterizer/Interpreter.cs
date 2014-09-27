148acc94560d4cf2f62c0c3b54b908d64034e195
Samuel Carlsson <samuel.carlsson@gmail.com>
2014-09-28 01:08:29 +0200

    Adding basic interpreter

---
+++ NRasterizer/Interpreter.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NRasterizer
+{
+    internal class GraphicState
+    {
+    }
+
+    internal abstract class Instruction
+    {
+        public abstract bool Matches(byte opcode);
+        public virtual int EatInstructionStream(byte[] instructions, int ip, Stack<UInt32> stack) { return ip; }
+        public abstract void Execute(GraphicState state, Stack<UInt32> stack, params bool[] flags);
+    }
+    
+    internal class NPushB : Instruction
+    {
+        public override bool Matches(byte opcode) { return opcode == 0x40; }
+        public override int EatInstructionStream(byte[] instructions, int ip, Stack<UInt32> stack)
+        {
+            byte n = instructions[ip++];
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(instructions[ip++]); // TODO: instructions may overrun?
+            }
+            return ip;
+        }
+        public override void Execute(GraphicState state, Stack<UInt32> stack, params bool[] flags)
+        {
+        }
+    }
+
+    internal class MDAP : Instruction
+    {
+        public override bool Matches(byte opcode) { return (opcode & 0xfe) == 0x2e; }
+        public override void Execute(GraphicState state, Stack<UInt32> stack, params bool[] flags)
+        {
+        }
+    }
+
+    internal class Interpreter
+    {
+        private readonly Instruction[] _lookup;
+
+        public Interpreter()
+        {
+            var instructions = new List<Instruction>();
+            instructions.Add(new NPushB());
+            _lookup = BuildLookup(instructions);
+        }
+
+        private Instruction[] BuildLookup(List<Instruction> instructions)
+        {
+            var result = new List<Instruction>(256);
+            return result.ToArray();
+        }
+
+        public void Run(byte[] instructions)
+        {
+            var stack = new Stack<UInt32>();
+            var state = new GraphicState();
+            for (int ip = 0; ip < instructions.Length; ip++)
+            {
+                var opcode = instructions[ip];
+                var instruction = _lookup[opcode];
+                ip = instruction.EatInstructionStream(instructions, ip, stack);
+                instruction.Execute(state, stack);
+            }
+        }
+    }
+}
