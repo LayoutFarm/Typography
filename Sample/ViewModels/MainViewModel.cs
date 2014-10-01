a4ee442db02cc6af56a7f09090bf7c832e3dd1d2
Samuel Carlsson <samuel.carlsson@volumental.com>
2014-10-01 12:26:43 +0200

    Adding font size control.

--- Sample/ViewModels/MainViewModel.cs
+++ Sample/ViewModels/MainViewModel.cs
@@ -16,20 +16,20 @@ namespace Sample.ViewModels
         private Typeface _typeface;
 
         private List<Point> _points = new List<Point>();
-        private int _selected;
+        private int _size;
         private string _text = "s";
 
         private readonly WriteableBitmap _raster = new WriteableBitmap(320, 240, 72, 72, System.Windows.Media.PixelFormats.Gray8, null);
 
-        public List<Point> Points
-        {
-            get { return _points; }
-            private set
-            {
-                _points = value;
-                RaisePropertyChanged();
-            }
-        }
+        //public List<Point> Points
+        //{
+        //    get { return _points; }
+        //    private set
+        //    {
+        //        _points = value;
+        //        RaisePropertyChanged();
+        //    }
+        //}
 
         public Typeface Typeface
         {
@@ -41,34 +41,32 @@ namespace Sample.ViewModels
             }
         }
 
-        private IEnumerable<Point> ToPoints(Glyph glyph, Bounds source, Rect target)
-        {
-            var allX = glyph.X;
-            var allY = glyph.Y;
-            for (int i = 0; i < glyph.X.Length; i++)
-            {
-                float x = allX[i];
-                float y = allY[i];
-                var p = new Point(
-                    (x - source.XMin) / (source.XMax - source.XMin),
-                    1.0 - ((y - source.YMin) / (source.YMax - source.YMin)));
-                yield return p;
-            }
-        }
+        //private IEnumerable<Point> ToPoints(Glyph glyph, Bounds source, Rect target)
+        //{
+        //    var allX = glyph.X;
+        //    var allY = glyph.Y;
+        //    for (int i = 0; i < glyph.X.Length; i++)
+        //    {
+        //        float x = allX[i];
+        //        float y = allY[i];
+        //        var p = new Point(
+        //            (x - source.XMin) / (source.XMax - source.XMin),
+        //            1.0 - ((y - source.YMin) / (source.YMax - source.YMin)));
+        //        yield return p;
+        //    }
+        //}
 
-        public int SelectedGlyph
+        public int FontSize
         {
-            get { return _selected; }
+            get { return _size; }
             set
             {
-                _selected = value;
+                _size = value;
+                Draw(_text, _size);
                 RaisePropertyChanged();
-
-                // Update points
-                Points = ToPoints(_typeface.Glyphs[_selected], _typeface.Bounds, Rect.Empty).ToList();
             }
         }
-
+        
         public ICommand Load { get { return new DelegatingCommand(LoadTypeface); } }
         //public ICommand Rasterize { get { return new DelegatingCommand(RasterizeGlyph); } }
         public ICommand SaveRaster { get { return new DelegatingCommand(SaveRasterToFile); } }
@@ -99,7 +97,7 @@ namespace Sample.ViewModels
             set
             {
                 _text = value;
-                Draw(_text);
+                Draw(_text, _size);
                 RaisePropertyChanged();
             }
         }
@@ -109,13 +107,13 @@ namespace Sample.ViewModels
             return new Int32Rect(0, 0, bitmap.PixelHeight, bitmap.PixelHeight);
         }
 
-        private void Draw(string s)
+        private void Draw(string text, int size)
         {
             //SelectedGlyph = _typeface.LookupIndex((char)0x0041); // A
 
             var raster = new Raster(_raster.PixelWidth, _raster.PixelHeight, _raster.PixelWidth, 72);
             var r = new Rasterizer(_typeface);
-            r.Rasterize(s, 32, raster);
+            r.Rasterize(text, size, raster);
 
             _raster.WritePixels(Bounds(_raster), raster.Pixels, raster.Stride, 0);
         }
