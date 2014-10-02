1c413caa94b0a8498d4b70bf65b34e17aed37e57
Samuel Carlsson <samuel.carlsson@gmail.com>
2014-10-02 15:19:44 +0200

    Rasterizing implicit beziers

--- NRasterizer/Glyph.cs
+++ NRasterizer/Glyph.cs
@@ -1,7 +1,26 @@
-﻿using System.Collections.Generic;
+﻿using System.Linq;
+using System.Collections.Generic;
 
 namespace NRasterizer
 {
+    internal class Point
+    {
+        private readonly short _x;
+        private readonly short _y;
+        private readonly bool _on;
+
+        public Point(short x, short y, bool on)
+        {
+            _x = x;
+            _y = y;
+            _on = on;
+        }
+
+        public short X { get { return _x; } }
+        public short Y { get { return _y; } }
+        public bool On { get { return _on; } }
+    }
+    
     public class Glyph
     {
         private readonly short[] _x;
@@ -25,31 +44,69 @@ namespace NRasterizer
 
         public int ContourCount { get { return _contourEndPoints.Length; } }
 
+        private Point At(int pointIndex)
+        {
+            return new Point(_x[pointIndex], _y[pointIndex], _on[pointIndex]);
+        }
+
+        private IEnumerable<Point> GetContourPoints(int contourIndex)
+        {
+            var begin = GetContourBegin(contourIndex);
+            var end = GetContourEnd(contourIndex);
+            for (int i = begin; i < end; i++)
+            {
+                yield return At(i);
+            }
+        }
+
+        private IEnumerable<Point> InsertImplicit(IEnumerable<Point> points)
+        {
+            var previous = points.First();
+            yield return previous;
+            foreach (var p in points.Skip(1))
+            {
+                if (!previous.On && !p.On)
+                {
+                    // implicit point on curve
+                    yield return new Point((short)((previous.X + p.X) / 2), (short)((previous.Y + p.Y) / 2), true);
+                }
+                previous = p;
+                yield return p;
+            }
+        }
+
+        private T Circular<T>(List<T> list, int index)
+        {
+            return list[index % list.Count];
+        }
+
         internal IEnumerable<Segment> GetContourIterator(int contourIndex,
             int fontX, int fontY,
             float xOffset, float yOffset, float scaleX, float scaleY)
         {
+            var pts = InsertImplicit(GetContourPoints(contourIndex)).ToList();
+
             var begin = GetContourBegin(contourIndex);
             var end = GetContourEnd(contourIndex);
-            for (int i = begin; i < end; i += _on[(i+1) % _on.Length] ? 1 : 2)
+            for (int i = 0; i < end - begin; i += pts[(i + 1) % pts.Count].On ? 1 : 2)
             {
-                if (_on[(i + 1) % _on.Length])
+                if (pts[(i + 1) % pts.Count].On)
                 {
                     yield return new Line(
-                        (int)(xOffset + (fontX + _x[i]) * scaleX),
-                        (int)(yOffset + (fontY + _y[i]) * scaleY),
-                        (int)(xOffset + (fontX + _x[i + 1]) * scaleX),
-                        (int)(yOffset + (fontY + _y[i + 1]) * scaleY));
+                        (int)(xOffset + (fontX + pts[i].X) * scaleX),
+                        (int)(yOffset + (fontY + pts[i].Y) * scaleY),
+                        (int)(xOffset + (fontX + Circular(pts, i + 1).X) * scaleX),
+                        (int)(yOffset + (fontY + Circular(pts, i + 1).Y) * scaleY));
                 }
                 else
                 {
                     yield return new Bezier(
-                        xOffset + (fontX + _x[i]) * scaleX,
-                        yOffset + (fontY + _y[i]) * scaleY,
-                        xOffset + (fontX + _x[i + 1]) * scaleX,
-                        yOffset + (fontY + _y[i + 1]) * scaleY,
-                        xOffset + (fontX + _x[(i + 2) % _y.Length]) * scaleX,
-                        yOffset + (fontY + _y[(i + 2) % _y.Length]) * scaleY);
+                        xOffset + (fontX + pts[i].X) * scaleX,
+                        yOffset + (fontY + pts[i].Y) * scaleY,
+                        xOffset + (fontX + Circular(pts, i + 1).X) * scaleX,
+                        yOffset + (fontY + Circular(pts, i + 1).Y) * scaleY,
+                        xOffset + (fontX + Circular(pts, i + 2).X) * scaleX,
+                        yOffset + (fontY + Circular(pts, i + 2).Y) * scaleY);
                 }
             }
             // TODO: What if the last segment if a bezier
