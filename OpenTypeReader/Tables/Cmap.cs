4d0dc38aeb987cdc8d90f7a83e45d250a5f2fce4
win <wintercoredev@gmail.com>
2016-10-21 07:47:15 +0700

    63: simplify how to count glyphIdArray record num

--- OpenTypeReader/Tables/Cmap.cs
+++ OpenTypeReader/Tables/Cmap.cs
@@ -7,18 +7,20 @@ namespace NRasterizer.Tables
 {
     class Cmap : TableEntry
     {
-        List<CharacterMap> charMaps;
+        CharacterMap[] charMaps;
         public override string Name
         {
             get { return "cmap"; }
         }
-        public List<CharacterMap> CharMaps
+        public CharacterMap[] CharMaps
         {
             get { return charMaps; }
         }
         protected override void ReadContentFrom(BinaryReader input)
         {
             //https://www.microsoft.com/typography/otspec/cmap.htm
+            long beginAt = input.BaseStream.Position;
+            //
             ushort version = input.ReadUInt16(); // 0
             ushort tableCount = input.ReadUInt16();
             var entries = new List<CMapEntry>(tableCount);
@@ -30,15 +32,13 @@ namespace NRasterizer.Tables
                 entries.Add(new CMapEntry(platformId, encodingId, offset));
             }
 
-
-            charMaps = new List<CharacterMap>(tableCount);
-            uint tableOffset = this.Header.Offset;
-            foreach (CMapEntry entry in entries)
+            charMaps = new CharacterMap[tableCount];
+            for (int i = 0; i < tableCount; i++)
             {
-                //TODO: review here
-                input.BaseStream.Seek(tableOffset, SeekOrigin.Begin);//reset
-                input.BaseStream.Seek(entry.Offset, SeekOrigin.Current);
-                charMaps.Add(ReadCharacterMap(entry, input));
+                CMapEntry entry = entries[i];
+                input.BaseStream.Seek(beginAt + entry.Offset, SeekOrigin.Begin);
+                charMaps[i] = (ReadCharacterMap(entry, input));
+
             }
         }
         static UInt16[] ReadUInt16Array(BinaryReader input, int length)
@@ -54,7 +54,7 @@ namespace NRasterizer.Tables
         static CharacterMap ReadCharacterMap(CMapEntry entry, BinaryReader input)
         {
             // I want to thank Microsoft for not giving a simple count on the glyphIdArray
-            long tableStart = input.BaseStream.Position;
+
             ushort format = input.ReadUInt16();
             ushort length = input.ReadUInt16();
             switch (format)
@@ -65,16 +65,12 @@ namespace NRasterizer.Tables
                     }
                 case 4:
                     {
-                        return ReadCmapFormat4(input, tableStart);
+                        return ReadCmapFormat4(input, length);
                     }
             }
         }
-        /// <summary>
-        ///  Format 4: Segment mapping to delta values
-        /// </summary>
-        /// <param name="input"></param>
-        /// <param name="tableStart"></param>
-        static CharacterMap ReadCmapFormat4(BinaryReader input, long tableStart)
+
+        static CharacterMap ReadCmapFormat4(BinaryReader input, ushort cmapLen)
         {
             //This is the Microsoft standard character to glyph index mapping table for fonts that support Unicode ranges other than the range [U+D800 - U+DFFF] (defined as Surrogates Area, in Unicode v 3.0) 
             //which is used for UCS-4 characters.
@@ -87,6 +83,8 @@ namespace NRasterizer.Tables
             //    A four-word header gives parameters for an optimized search of the segment list;
             //    Four parallel arrays describe the segments (one segment for each contiguous range of codes);
             //    A variable-length array of glyph IDs (unsigned words).
+            long tableStartEndAt = input.BaseStream.Position + cmapLen;
+
             ushort version = input.ReadUInt16();
             ushort segCountX2 = input.ReadUInt16(); //2 * segCount
             ushort searchRange = input.ReadUInt16(); //2 * (2**FLOOR(log2(segCount)))
@@ -100,33 +98,32 @@ namespace NRasterizer.Tables
             input.ReadUInt16(); // Reserved = 0               
             ushort[] startCode = ReadUInt16Array(input, segCount); //Starting character code for each segment
             ushort[] idDelta = ReadUInt16Array(input, segCount); //Delta for all character codes in segment
-            ushort[] idRangeOffset = ReadUInt16Array(input, segCount); //Offset in bytes to glyph indexArray, or 0 
-            int glyphIdArrayRecordNum = FindGlyphIdArrayLenInBytes(idRangeOffset) / 2;
+            ushort[] idRangeOffset = ReadUInt16Array(input, segCount); //Offset in bytes to glyph indexArray, or 0   
             //------------------------------------------------------------------------------------ 
-            // I want to thank Microsoft for not giving a simple count on the glyphIdArray
-            //int glyphIdArrayLength = (int)((input.BaseStream.Position - tableStart) / sizeof(UInt16)); //not correct 
-            ushort[] glyphIdArray = ReadUInt16Array(input, glyphIdArrayRecordNum);//Glyph index array 
+            long remainingLen = tableStartEndAt - input.BaseStream.Position;
+            int recordNum2 = (int)(remainingLen / 2);
+            ushort[] glyphIdArray = ReadUInt16Array(input, recordNum2);//Glyph index array 
             return new CharacterMap(segCount, startCode, endCode, idDelta, idRangeOffset, glyphIdArray);
         }
-        static int FindGlyphIdArrayLenInBytes(ushort[] idRangeOffset)
-        {
-            //1. find max OffsetValue (in bytes unit)
-            //this is the possible value to reach from the idRangeOffsetRecord 
-            ushort max = 0;
-            int foundAt = 0;
-            for (int i = idRangeOffset.Length - 1; i >= 0; --i)
-            {
-                ushort off = idRangeOffset[i];
-                if (off > max)
-                {
-                    max = off;
-                    foundAt = i;
-                }
-            }
-            //----------------------------
-            //2. then offset with current found record
-            return max - (foundAt * 2); //*2 = to byte unit 
-        }
+        //static int FindGlyphIdArrayLenInBytes(ushort[] idRangeOffset)
+        //{
+        //    //1. find max OffsetValue (in bytes unit)
+        //    //this is the possible value to reach from the idRangeOffsetRecord 
+        //    ushort max = 0;
+        //    int foundAt = 0;
+        //    for (int i = idRangeOffset.Length - 1; i >= 0; --i)
+        //    {
+        //        ushort off = idRangeOffset[i];
+        //        if (off > max)
+        //        {
+        //            max = off;
+        //            foundAt = i;
+        //        }
+        //    }
+        //    //----------------------------
+        //    //2. then offset with current found record
+        //    return max - (foundAt * 2); //*2 = to byte unit 
+        //}
         class CMapEntry
         {
             readonly UInt16 _platformId;
