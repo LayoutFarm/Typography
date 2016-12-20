ca8da2f7a143c152b82b2e3560f07c480fe913bd
win <wintercoredev@gmail.com>
2016-12-20 16:18:02 +0700

    122: init

---
+++ NetCore/PixelFarm.OpenType/TrueTypeInterperter/TrueTypeInterpreter.cs
@@ -0,0 +1,9 @@
+﻿//MIT, 2015, Michael Popoloski MIT, from https://github.com/MikePopoloski/SharpFont
+
+using System;
+namespace PixelFarm.OpenType
+{
+    public class TrueTypeInterpreter
+    {
+    }
+}
