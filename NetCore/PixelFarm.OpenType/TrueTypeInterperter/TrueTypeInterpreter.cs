e4a27bb483ac79daa8cf42d1df3a052d52e0ed98
win <wintercoredev@gmail.com>
2016-12-22 00:13:14 +0700

    132: just move source files

--- NetCore/PixelFarm.OpenType/TrueTypeInterperter/TrueTypeInterpreter.cs
+++
@@ -1,9 +0,0 @@
-﻿//MIT, 2015, Michael Popoloski MIT, from https://github.com/MikePopoloski/SharpFont
-
-using System;
-namespace PixelFarm.OpenType
-{
-    public class TrueTypeInterpreter
-    {
-    }
-}
