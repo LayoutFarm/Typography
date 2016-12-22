0c80c19210b3b6391fb4b57d37faf6077c0a9851
win <wintercoredev@gmail.com>
2016-12-22 08:07:52 +0700

    136: remove serialize info

--- NetCore/PixelFarm.OpenType/TrueTypeInterperter/InvalidFontException.cs
+++ NetCore/PixelFarm.OpenType/TrueTypeInterperter/InvalidFontException.cs
@@ -1,7 +1,6 @@
 ﻿//MIT, 2015, Michael Popoloski's SharpFont
 
 using System;
-using System.Runtime.Serialization;
 
 namespace NOpenType
 {
@@ -34,16 +33,6 @@ namespace NOpenType
         {
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="InvalidFontException"/> class.
-        /// </summary>
-        /// <param name="info">The <see cref="T:System.Runtime.Serialization.SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
-        /// <param name="context">The <see cref="T:System.Runtime.Serialization.StreamingContext"/> that contains contextual information about the source or destination.</param>
-        /// <exception cref="T:System.ArgumentNullException">The <paramref name="info"/> parameter is null. </exception>
-        /// <exception cref="T:System.Runtime.Serialization.SerializationException">The class name is null or <see cref="P:System.Exception.HResult"/> is zero (0). </exception>
-        protected InvalidFontException(SerializationInfo info, StreamingContext context)
-            : base(info, context)
-        {
-        }
+        
     }
 }
