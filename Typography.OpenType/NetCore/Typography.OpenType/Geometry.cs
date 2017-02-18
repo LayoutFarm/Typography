eb451f8424ebda5d17c9d0425497cd482247ba9c
win <wintercoredev@gmail.com>
2017-02-18 09:45:14 +0700

    257: GlyphPointF

--- Typography.OpenType/NetCore/Typography.OpenType/Geometry.cs
+++ Typography.OpenType/NetCore/Typography.OpenType/Geometry.cs
@@ -8,8 +8,8 @@ namespace Typography.OpenType
 
     public struct GlyphPointF
     {
-        public Vector2 P;
-        public bool onCurve;
+        internal Vector2 P;
+        internal bool onCurve;
         public GlyphPointF(float x, float y, bool onCurve)
         {
             P = new Vector2(x, y);
@@ -20,33 +20,33 @@ namespace Typography.OpenType
             P = position;
             this.onCurve = onCurve;
         }
-
-        public GlyphPointF Offset(Vector2 offset) { return new GlyphPointF(P + offset, onCurve); }
-        public GlyphPointF Offset(short dx, short dy) { return new GlyphPointF(new Vector2(P.X + dx, P.Y + dy), onCurve); }
-
-        public void ApplyScale(float scale)
-        {
-            P *= scale;
-        }
-        public void ApplyScaleOnlyOnXAxis(float scale)
-        {
-            P = new Vector2(P.X * scale, P.Y);
-        }
-
-        public override string ToString() { return P.ToString() + " " + onCurve.ToString(); }
-
-        public static implicit operator Vector2(GlyphPointF p) { return p.P; }
-
+        public float X { get { return this.P.X; } }
+        public float Y { get { return this.P.Y; } }
         public static GlyphPointF operator *(GlyphPointF p, float n)
         {
             return new GlyphPointF(p.P * n, p.onCurve);
         }
-        public bool IsEqualsWith(GlyphPointF another)
+
+        //-----------------------------------------
+
+        internal GlyphPointF Offset(short dx, short dy) { return new GlyphPointF(new Vector2(P.X + dx, P.Y + dy), onCurve); }
+
+        internal void ApplyScale(float scale)
+        {
+            P *= scale;
+        }
+        internal void ApplyScaleOnlyOnXAxis(float scale)
+        {
+            P = new Vector2(P.X * scale, P.Y);
+        }
+
+#if DEBUG
+        internal bool dbugIsEqualsWith(GlyphPointF another)
         {
             return this.P == another.P && this.onCurve == another.onCurve;
         }
-        public float X { get { return this.P.X; } }
-        public float Y { get { return this.P.Y; } }
+        public override string ToString() { return P.ToString() + " " + onCurve.ToString(); }
+#endif
     }
 
 
