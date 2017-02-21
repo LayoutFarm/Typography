8586885306772a88c0d645233b372c5cce6c2661
win <wintercoredev@gmail.com>
2017-02-22 06:47:56 +0700

    290.1

--- Typography.OpenFont/NetCore/Typography.OpenFont/IGlyphPathBuilder.cs
+++ Typography.OpenFont/NetCore/Typography.OpenFont/IGlyphPathBuilder.cs
@@ -181,4 +181,45 @@ namespace Typography.OpenFont
                  ((v1.Y + v2y) / 2f));
         }
     }
+
+    //static int s_POINTS_PER_INCH = 72; //default value, 
+    //static int s_PIXELS_PER_INCH = 96; //default value
+    //public static float ConvEmSizeInPointsToPixels(float emsizeInPoint)
+    //{
+    //    return (int)(((float)emsizeInPoint / (float)s_POINTS_PER_INCH) * (float)s_PIXELS_PER_INCH);
+    //}
+
+    ////from http://www.w3schools.com/tags/ref_pxtoemconversion.asp
+    ////set default
+    //// 16px = 1 em
+    ////-------------------
+    ////1. conv font design unit to em
+    //// em = designUnit / unit_per_Em       
+    ////2. conv font design unit to pixels 
+    //// float scale = (float)(size * resolution) / (pointsPerInch * _typeface.UnitsPerEm);
+
+    ////-------------------
+    ////https://www.microsoft.com/typography/otspec/TTCH01.htm
+    ////Converting FUnits to pixels
+    ////Values in the em square are converted to values in the pixel coordinate system by multiplying them by a scale. This scale is:
+    ////pointSize * resolution / ( 72 points per inch * units_per_em )
+    ////where pointSize is the size at which the glyph is to be displayed, and resolution is the resolution of the output device.
+    ////The 72 in the denominator reflects the number of points per inch.
+    ////For example, assume that a glyph feature is 550 FUnits in length on a 72 dpi screen at 18 point. 
+    ////There are 2048 units per em. The following calculation reveals that the feature is 4.83 pixels long.
+    ////550 * 18 * 72 / ( 72 * 2048 ) = 4.83
+    ////-------------------
+    //public static float ConvFUnitToPixels(ushort reqFUnit, float fontSizeInPoint, ushort unitPerEm)
+    //{
+    //    //reqFUnit * scale             
+    //    return reqFUnit * GetFUnitToPixelsScale(fontSizeInPoint, unitPerEm);
+    //}
+    //public static float GetFUnitToPixelsScale(float fontSizeInPoint, ushort unitPerEm)
+    //{
+    //    //reqFUnit * scale             
+    //    return ((fontSizeInPoint * s_PIXELS_PER_INCH) / (s_POINTS_PER_INCH * unitPerEm));
+    //}
+
+
+
 }
