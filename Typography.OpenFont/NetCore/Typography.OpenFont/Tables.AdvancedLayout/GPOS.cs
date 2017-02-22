6edf2ac91c29ae4d1ce650d0aa84d69b597ce969
win <wintercoredev@gmail.com>
2017-02-22 09:35:30 +0700

    300: fixed GPOS:LkSubTableType6

--- Typography.OpenFont/NetCore/Typography.OpenFont/Tables.AdvancedLayout/GPOS.cs
+++ Typography.OpenFont/NetCore/Typography.OpenFont/Tables.AdvancedLayout/GPOS.cs
@@ -219,7 +219,10 @@ namespace Typography.OpenFont.Tables
                 int j = subTables.Count;
                 for (int i = 0; i < j; ++i)
                 {
+
                     subTables[i].DoGlyphPosition(inputGlyphs, startAt, len);
+                    //update len
+                    len = inputGlyphs.Count;
                 }
             }
             public List<LookupSubTable> SubTables { get { return subTables; } }
@@ -749,10 +752,18 @@ namespace Typography.OpenFont.Tables
                 public Mark2ArrayTable Mark2ArrayTable { get; set; } // Mark2 attachment points used to attach Mark1 glyphs to a specific Mark2 glyph. 
                 public override void DoGlyphPosition(List<GlyphPos> inputGlyphs, int startAt, int len)
                 {
-                    //find marker
-                    int x = 0;
-                    int j = inputGlyphs.Count;
-                    for (int i = 1; i < j; ++i) //start at 1
+                    //find marker 
+                    if (startAt == 0)
+                    {
+                        startAt++;
+                    }
+                    int lim = startAt + len;
+                    if (lim > inputGlyphs.Count)
+                    {
+                        lim = inputGlyphs.Count;
+                    }
+                    //
+                    for (int i = startAt; i < lim; ++i) //start at 1
                     {
                         GlyphPos glyphPos = inputGlyphs[i];
                         int markFound = MarkCoverage1.FindPosition(glyphPos.glyphIndex);
@@ -777,14 +788,14 @@ namespace Typography.OpenFont.Tables
                                     if (actualBasePos > -1)
                                     {
                                         GlyphPos prev_pos2 = inputGlyphs[actualBasePos];
-                                        glyphPos.xoffset += (short)((prev_pos2.xoffset + mark2BaseAnchor.xcoord - mark1Anchor.xcoord)); 
+                                        glyphPos.xoffset += (short)((prev_pos2.xoffset + mark2BaseAnchor.xcoord - mark1Anchor.xcoord));
                                     }
                                 }
                                 else
                                 {
                                     glyphPos.yoffset += (short)(mark1Anchor.ycoord);
                                     glyphPos.xoffset += (short)((prev_pos.xoffset + mark2BaseAnchor.xcoord - mark1Anchor.xcoord));
-                                } 
+                                }
                             }
                         }
                     }
