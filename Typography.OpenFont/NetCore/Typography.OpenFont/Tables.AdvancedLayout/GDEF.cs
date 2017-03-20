efe9aca2884109413904b63148c110234d2974ac
win <wintercoredev@gmail.com>
2017-03-20 20:12:41 +0700

    421: GDEF

--- Typography.OpenFont/NetCore/Typography.OpenFont/Tables.AdvancedLayout/GDEF.cs
+++ Typography.OpenFont/NetCore/Typography.OpenFont/Tables.AdvancedLayout/GDEF.cs
@@ -107,11 +107,11 @@ namespace Typography.OpenFont.Tables
             this.MajorVersion = reader.ReadUInt16();
             this.MinorVersion = reader.ReadUInt16();
             //
-            short glyphClassDefOffset = reader.ReadInt16();
-            short attachListOffset = reader.ReadInt16();
-            short ligCaretListOffset = reader.ReadInt16();
-            short markAttachClassDefOffset = reader.ReadInt16();
-            short markGlyphSetsDefOffset = 0;
+            ushort glyphClassDefOffset = reader.ReadUInt16();
+            ushort attachListOffset = reader.ReadUInt16();
+            ushort ligCaretListOffset = reader.ReadUInt16();
+            ushort markAttachClassDefOffset = reader.ReadUInt16();
+            ushort markGlyphSetsDefOffset = 0;
             uint itemVarStoreOffset = 0;
             //
             switch (MinorVersion)
@@ -119,10 +119,10 @@ namespace Typography.OpenFont.Tables
                 default: throw new NotSupportedException();
                 case 0: break;
                 case 2:
-                    markGlyphSetsDefOffset = reader.ReadInt16();
+                    markGlyphSetsDefOffset = reader.ReadUInt16();
                     break;
                 case 3:
-                    markGlyphSetsDefOffset = reader.ReadInt16();
+                    markGlyphSetsDefOffset = reader.ReadUInt16();
                     itemVarStoreOffset = reader.ReadUInt32();
                     break;
             }
