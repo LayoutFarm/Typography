cf3304d22b17bbdbd6d6cf4d98c25ef2f5ed1c67
win <wintercoredev@gmail.com>
2017-05-19 23:21:38 +0700

    522: PixelFarm.Typography

---
+++ PixelFarm.Typography/1.1_Drawing_Fonts/PixelScaleLayoutEngine.cs
@@ -0,0 +1,385 @@
+﻿//MIT, 2016-2017, WinterDev
+
+using System;
+using System.Collections.Generic;
+using PixelFarm.Agg;
+
+using PixelFarm.Drawing.Fonts;
+
+using Typography.OpenFont;
+using Typography.TextLayout;
+
+
+namespace Typography.Contours
+{
+
+
+    public struct GlyphControlParameters
+    {
+        public float avgXOffsetToFit;
+        public short minX;
+        public short minY;
+        public short maxX;
+        public short maxY;
+
+    }
+    class GlyphMeshStore
+    {
+
+        class GlyphMeshData
+        {
+            public GlyphDynamicOutline dynamicOutline;
+            public VertexStore vxsStore;
+            public float avgXOffsetToFit;
+            public Bounds orgBounds;
+
+            public GlyphControlParameters GetControlPars()
+            {
+                var pars = new GlyphControlParameters();
+                pars.minX = orgBounds.XMin;
+                pars.minY = orgBounds.YMin;
+                pars.maxX = orgBounds.XMax;
+                pars.maxY = orgBounds.YMax;
+                pars.avgXOffsetToFit = avgXOffsetToFit;
+                return pars;
+            }
+
+        }
+        /// <summary>
+        /// store typeface and its builder
+        /// </summary>
+        Dictionary<Typeface, GlyphPathBuilder> _cacheGlyphPathBuilders = new Dictionary<Typeface, GlyphPathBuilder>();
+        /// <summary>
+        /// glyph mesh data for specific condition
+        /// </summary>
+        GlyphMeshCollection<GlyphMeshData> _hintGlyphCollection = new GlyphMeshCollection<GlyphMeshData>();
+
+        GlyphPathBuilder _currentGlyphBuilder;
+        Typeface _currentTypeface;
+        float _currentFontSizeInPoints;
+        HintTechnique _currentHintTech;
+
+        VertexStorePool _vxsPool = new VertexStorePool(); //TODO: review pool again
+        GlyphTranslatorToVxs _tovxs = new GlyphTranslatorToVxs();
+
+        public GlyphMeshStore()
+        {
+
+        }
+        public void SetHintTechnique(HintTechnique hintTech)
+        {
+            _currentHintTech = hintTech;
+
+        }
+
+        /// <summary>
+        /// set current font
+        /// </summary>
+        /// <param name="typeface"></param>
+        /// <param name="fontSizeInPoints"></param>
+        public void SetFont(Typeface typeface, float fontSizeInPoints)
+        {
+            if (_currentGlyphBuilder != null && !_cacheGlyphPathBuilders.ContainsKey(typeface))
+            {
+                //store current typeface to cache
+                _cacheGlyphPathBuilders[_currentTypeface] = _currentGlyphBuilder;
+            }
+            _currentTypeface = typeface;
+            _currentGlyphBuilder = null;
+            if (typeface == null) return;
+
+            //----------------------------
+            //check if we have this in cache ?
+            //if we don't have it, this _currentTypeface will set to null ***                  
+            _cacheGlyphPathBuilders.TryGetValue(_currentTypeface, out _currentGlyphBuilder);
+            if (_currentGlyphBuilder == null)
+            {
+                _currentGlyphBuilder = new GlyphPathBuilder(typeface);
+            }
+            //----------------------------------------------
+            this._currentFontSizeInPoints = fontSizeInPoints;
+            //------------------------------------------ 
+            _hintGlyphCollection.SetCacheInfo(typeface, this._currentFontSizeInPoints, _currentHintTech);
+        }
+        /// <summary>
+        /// get existing or create new one from current font setting
+        /// </summary>
+        /// <param name="glyphIndex"></param>
+        /// <returns></returns>
+        GlyphMeshData InternalGetGlyphMesh(ushort glyphIndex)
+        {
+            GlyphMeshData glyphMeshData;
+            if (!_hintGlyphCollection.TryGetCacheGlyph(glyphIndex, out glyphMeshData))
+            {
+                //if not found then create new glyph vxs and cache it
+                _currentGlyphBuilder.SetHintTechnique(_currentHintTech);
+                _currentGlyphBuilder.BuildFromGlyphIndex(glyphIndex, _currentFontSizeInPoints);
+                GlyphDynamicOutline dynamicOutline = _currentGlyphBuilder.LatestGlyphFitOutline;
+                //-----------------------------------  
+                glyphMeshData = new GlyphMeshData();
+                glyphMeshData.avgXOffsetToFit = dynamicOutline.AvgXFitOffset;
+                glyphMeshData.orgBounds = dynamicOutline.OriginalGlyphControlBounds;
+                glyphMeshData.dynamicOutline = dynamicOutline;
+                Bounds orgGlyphBounds = dynamicOutline.OriginalGlyphControlBounds;
+
+                _hintGlyphCollection.RegisterCachedGlyph(glyphIndex, glyphMeshData);
+                //-----------------------------------    
+            }
+            return glyphMeshData;
+        }
+        /// <summary>
+        /// get glyph left offset-to-fit value from current font setting
+        /// </summary>
+        /// <param name="glyphIndex"></param>
+        /// <returns></returns>
+        public GlyphControlParameters GetControlPars(ushort glyphIndex)
+        {
+            return InternalGetGlyphMesh(glyphIndex).GetControlPars();
+        }
+
+        /// <summary>
+        /// get glyph mesh from current font setting
+        /// </summary>
+        /// <param name="glyphIndex"></param>
+        /// <returns></returns>
+        public VertexStore GetGlyphMesh(ushort glyphIndex)
+        {
+            GlyphMeshData glyphMeshData = InternalGetGlyphMesh(glyphIndex);
+            if (glyphMeshData.vxsStore == null)
+            {
+                //build vxs
+                _tovxs.Reset();
+
+                float pxscale = _currentTypeface.CalculateToPixelScaleFromPointSize(_currentFontSizeInPoints);
+                GlyphDynamicOutline dynamicOutline = glyphMeshData.dynamicOutline;
+                dynamicOutline.GenerateOutput(_tovxs, pxscale);
+                glyphMeshData.vxsStore = new VertexStore();
+                //----------------
+                _tovxs.WriteOutput(glyphMeshData.vxsStore, _vxsPool);
+
+            }
+            return glyphMeshData.vxsStore;
+
+        }
+    }
+
+    class PixelScaleLayoutEngine : IPixelScaleLayout
+    {
+        Typeface _typeface;
+        GlyphMeshStore _hintedFontStore;
+        float _fontSizeInPoints;
+        public PixelScaleLayoutEngine()
+        {
+        }
+        public GlyphMeshStore HintedFontStore
+        {
+            get { return _hintedFontStore; }
+            set
+            {
+                _hintedFontStore = value;
+            }
+        }
+
+        public bool UseWithLcdSubPixelRenderingTechnique { get; set; }
+
+        public void SetFont(Typeface typeface, float fontSizeInPoints)
+        {
+            _typeface = typeface;
+            _fontSizeInPoints = fontSizeInPoints;
+        }
+
+
+        struct FineABC
+        {
+            //this struct is used for local calculation (in a method) only
+            //not suite for storing data / pass data between methods
+
+            /// <summary>
+            /// avg x to fit value, this is calculated value from dynamic layout
+            /// </summary>
+            public float s_avg_x_ToFit;
+
+            /// <summary>
+            /// scaled offsetX
+            /// </summary>
+            public float s_offsetX;
+            /// <summary>
+            /// scaled offsetY
+            /// </summary>
+            public float s_offsetY;
+            /// <summary>
+            ///  scaled advance width
+            /// </summary>
+            public float s_advW;
+            /// <summary>
+            /// scaled x min
+            /// </summary>
+            public float s_xmin;
+            /// <summary>
+            /// scaled x max
+            /// </summary>
+            public float s_xmax;
+            /// <summary>
+            /// scaled a part
+            /// </summary>
+            public float s_a;
+            /// <summary>
+            /// scaled c part
+            /// </summary>
+            public float s_c;
+
+            /// <summary>
+            /// approximate final advance width for this glyph
+            /// </summary>
+            public int final_advW;
+
+
+            public float c_diff;
+            public float s_xmax_to_final_advance;
+
+            public void SetData(float pxscale, GlyphControlParameters controlPars, short offsetX, short offsetY, ushort orgAdvW)
+            {
+
+                s_avg_x_ToFit = controlPars.avgXOffsetToFit;
+                float o_a = controlPars.minX;
+                float o_c = (short)(orgAdvW - controlPars.maxX);
+
+                if (o_c < 0)
+                {
+                    //TODO: review here ...
+                    //? 
+                    o_c = 0;
+                }
+                //-----------------
+                //calculate...  
+                s_offsetX = pxscale * offsetX;
+                s_offsetY = pxscale * offsetY;
+                s_advW = pxscale * orgAdvW;
+                s_xmin = pxscale * controlPars.minX;
+                s_xmax = pxscale * controlPars.maxX;
+                s_a = pxscale * o_a;
+                s_c = pxscale * o_c;
+                //--------------------------------------   
+                final_advW = ((s_advW - (int)s_advW) > 0.5) ?
+                                (int)(s_advW + 1) : //round
+                                (int)(s_advW);
+                s_xmax_to_final_advance = final_advW - s_xmax;
+                c_diff = final_advW - s_advW;
+            }
+
+        }
+        public void Layout(IGlyphPositions posStream, List<GlyphPlan> outputGlyphPlanList)
+        {
+
+            int finalGlyphCount = posStream.Count;
+            float pxscale = _typeface.CalculateToPixelScaleFromPointSize(this._fontSizeInPoints);
+            float onepx = 1 / pxscale;
+            //
+            double cx = 0;
+            short cy = 0;
+            //
+            //at this state, we need exact info at this specific pxscale
+            //
+            _hintedFontStore.SetFont(_typeface, this._fontSizeInPoints);
+            FineABC current_ABC = new FineABC();
+            FineABC prev_ABC = new FineABC();
+
+            for (int i = 0; i < finalGlyphCount; ++i)
+            {
+                short offsetX, offsetY, advW; //all from pen-pos
+                ushort glyphIndex = posStream.GetGlyph(i, out offsetX, out offsetY, out advW);
+                GlyphControlParameters controlPars = _hintedFontStore.GetControlPars(glyphIndex);
+                current_ABC.SetData(pxscale, controlPars, offsetX, offsetY, (ushort)advW);
+                //-------------------------------------------------------------
+                if (i > 0)
+                {
+                    //ideal interspace
+                    //float idealInterGlyphSpace = -prev_ABC.s_avgToFit + prev_ABC.s_c + current_ABC.s_a + current_ABC.s_avgToFit;
+                    //float idealInterGlyphSpace = -prev_ABC.s_avgToFit + prev_ABC.s_c + current_ABC.s_a + current_ABC.s_avgToFit;
+                    float idealInterGlyphSpace = prev_ABC.s_c + current_ABC.s_a;
+                    if (idealInterGlyphSpace > 1 - 0.5f)
+                    {
+                        //please ensure that we have interspace atleast 1px
+                        //if not we just insert 1 px  ***
+
+                        //TODO: review here,
+                        //0.66f come from  2/3f of a pixel  
+                        if (idealInterGlyphSpace < 1 + 0.66f)
+                        {
+                            float fine_h = -prev_ABC.s_avg_x_ToFit + prev_ABC.c_diff + current_ABC.s_a + current_ABC.s_avg_x_ToFit;
+                            if (fine_h < 0)
+                            {
+                                //need more space
+                                //i-o
+                                cx += 1;
+                            }
+                            else
+                            {
+
+                                if (fine_h > 1)
+                                {
+                                    //o-i
+                                    cx -= 1;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            if (-prev_ABC.s_avg_x_ToFit + current_ABC.s_avg_x_ToFit > 0.5f)
+                            {
+                                cx--;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        float idealInterGlyphSpace2 = -prev_ABC.s_avg_x_ToFit + prev_ABC.s_c + current_ABC.s_a + current_ABC.s_avg_x_ToFit;
+
+                        if (idealInterGlyphSpace2 < 0)
+                        {
+                            // eg i-j seq
+                            cx++;
+                        }
+                        else
+                        {
+
+                            if (prev_ABC.s_xmax_to_final_advance < 0)
+                            {
+                                //f-f
+                                cx++;
+                            }
+                        }
+                    }
+                }
+                //------------------------------------------------------------- 
+                float exact_x = (float)(cx + current_ABC.s_offsetX);
+                float exact_y = (float)(cy + current_ABC.s_offsetY);
+
+                //check if the current position can create a sharp glyph
+                int exact_x_floor = (int)exact_x;
+                float x_offset_to_fit = controlPars.avgXOffsetToFit;
+                //offset range that can produce sharp glyph (by observation)
+                //is between x_offset_to_fit - 0.3f to x_offset_to_fit + 0.3f 
+
+                float final_x = exact_x_floor + x_offset_to_fit;
+                if (UseWithLcdSubPixelRenderingTechnique)
+                {
+                    final_x += 0.33f;
+                }
+
+                outputGlyphPlanList.Add(new GlyphPlan(
+                    glyphIndex,
+                    final_x,
+                    exact_y,
+                    current_ABC.final_advW));
+                // 
+                //
+                cx += current_ABC.final_advW;
+                //-----------------------------------------------
+                prev_ABC = current_ABC;//add to prev
+
+                // Console.WriteLine(exact_x + "+" + (x_offset_to_fit) + "=>" + final_x);
+            }
+        }
+    }
+}
---
+++ PixelFarm.Typography/1.1_Drawing_Fonts/VxsTextPrinter.cs
@@ -0,0 +1,335 @@
+﻿//MIT, 2016-2017, WinterDev 
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+using PixelFarm.Agg;
+
+using Typography.Contours;
+using Typography.OpenFont;
+using Typography.Rendering;
+using Typography.TextLayout;
+
+namespace PixelFarm.Drawing.Fonts
+{
+
+    public class VxsTextPrinter : DevTextPrinterBase, ITextPrinter
+    {
+        /// <summary>
+        /// target canvas
+        /// </summary>
+        CanvasPainter canvasPainter;
+        IFontLoader _fontLoader;
+        RequestFont _reqFont;
+        //----------------------------------------------------------- 
+        GlyphLayout _glyphLayout = new GlyphLayout();
+        List<GlyphPlan> _outputGlyphPlans = new List<GlyphPlan>();
+        Typeface _currentTypeface;
+        PixelScaleLayoutEngine _pxScaleEngine;
+        GlyphMeshStore _glyphMeshStore;
+        //-----------------------------------------------------------
+        Dictionary<InstalledFont, Typeface> _cachedTypefaces = new Dictionary<InstalledFont, Typeface>();
+        //-----------------------------------------------------------
+
+        public VxsTextPrinter(CanvasPainter canvasPainter, IFontLoader fontLoader)
+        {
+            this.canvasPainter = canvasPainter;
+            this._fontLoader = fontLoader;
+
+            _glyphMeshStore = new GlyphMeshStore();
+            //
+            _pxScaleEngine = new PixelScaleLayoutEngine();
+            _pxScaleEngine.HintedFontStore = _glyphMeshStore;//share _glyphMeshStore with pixel-scale-layout-engine
+            //
+            _glyphLayout.PxScaleLayout = _pxScaleEngine; //assign the pxscale-layout-engine to main glyphLayout engine
+
+        }
+        public CanvasPainter TargetCanvasPainter { get; set; }
+        bool TryGetTypeface(InstalledFont instFont, out Typeface found)
+        {
+            return _cachedTypefaces.TryGetValue(instFont, out found);
+        }
+        void RegisterTypeface(InstalledFont instFont, Typeface typeface)
+        {
+            _cachedTypefaces[instFont] = typeface;
+        }
+        /// <summary>
+        /// for layout that use with our  lcd subpixel rendering technique 
+        /// </summary>
+        public bool UseWithLcdSubPixelRenderingTechnique
+        {
+            get { return _pxScaleEngine.UseWithLcdSubPixelRenderingTechnique; }
+            set
+            {
+                _pxScaleEngine.UseWithLcdSubPixelRenderingTechnique = value;
+            }
+        }
+        public void ChangeFont(RequestFont font)
+        {
+            //1.  resolve actual font file
+            this._reqFont = font;
+            InstalledFont installedFont = _fontLoader.GetFont(font.Name, font.Style.ConvToInstalledFontStyle());
+            Typeface foundTypeface;
+
+            if (!TryGetTypeface(installedFont, out foundTypeface))
+            {
+                //if not found then create a new one
+                //if not found
+                //create the new one
+                using (FileStream fs = new FileStream(installedFont.FontPath, FileMode.Open, FileAccess.Read))
+                {
+                    var reader = new OpenFontReader();
+                    foundTypeface = reader.Read(fs);
+                }
+                RegisterTypeface(installedFont, foundTypeface);
+            }
+
+            this.Typeface = foundTypeface;
+        }
+        public void ChangeFillColor(Color fontColor)
+        {
+            //change font color
+
+#if DEBUG
+            Console.Write("please impl change font color");
+#endif
+        }
+        public void ChangeStrokeColor(Color strokeColor)
+        {
+
+        }
+
+        protected override void OnFontSizeChanged()
+        {
+            //update some font matrics property   
+            Typeface currentTypeface = _currentTypeface;
+            if (currentTypeface != null)
+            {
+                float pointToPixelScale = currentTypeface.CalculateToPixelScaleFromPointSize(this.FontSizeInPoints);
+                this.FontAscendingPx = currentTypeface.Ascender * pointToPixelScale;
+                this.FontDescedingPx = currentTypeface.Descender * pointToPixelScale;
+                this.FontLineGapPx = currentTypeface.LineGap * pointToPixelScale;
+                this.FontLineSpacingPx = FontAscendingPx - FontDescedingPx + FontLineGapPx;
+            }
+
+        }
+        public override GlyphLayout GlyphLayoutMan
+        {
+            get
+            {
+                return _glyphLayout;
+            }
+        }
+
+        public override Typeface Typeface
+        {
+            get
+            {
+                return _currentTypeface;
+            }
+            set
+            {
+
+                if (_currentTypeface == value) return;
+                _currentTypeface = value;
+                OnFontSizeChanged();
+            }
+        }
+
+
+        public void PrepareStringForRenderVx(RenderVxFormattedString renderVx, char[] text, int startAt, int len)
+        {
+
+            //1. update some props.. 
+            //2. update current type face
+            UpdateTypefaceAndGlyphBuilder();
+            Typeface typeface = _currentTypeface;// _glyphPathBuilder.Typeface;
+            //3. layout glyphs with selected layout technique
+            //TODO: review this again, we should use pixel?
+
+            float pxscale = typeface.CalculateToPixelScaleFromPointSize(FontSizeInPoints);
+            _outputGlyphPlans.Clear();
+            _glyphLayout.Layout(typeface, text, startAt, len, _outputGlyphPlans);
+            TextPrinterHelper.CopyGlyphPlans(renderVx, _outputGlyphPlans, pxscale);
+        }
+
+        public override void DrawCaret(float x, float y)
+        {
+
+            //        public override void DrawCaret(float xpos, float ypos)
+            //        {
+            //            CanvasPainter p = this.TargetCanvasPainter;
+            //            PixelFarm.Drawing.Color prevColor = p.StrokeColor;
+            //            p.StrokeColor = PixelFarm.Drawing.Color.Red;
+            //            p.Line(xpos, ypos, xpos, ypos + this.FontAscendingPx);
+            //            p.StrokeColor = prevColor;
+            //        }
+
+            //throw new NotImplementedException();
+        }
+
+        void UpdateTypefaceAndGlyphBuilder()
+        {
+            //1. update _glyphPathBuilder for current typeface
+            UpdateGlyphLayoutSettings();
+        }
+        void UpdateGlyphLayoutSettings()
+        {
+            if (this._reqFont == null)
+            {
+                //this.ScriptLang = canvasPainter.CurrentFont.GetOpenFontScriptLang();
+                ChangeFont(canvasPainter.CurrentFont);
+            }
+
+            //2.1              
+            _glyphMeshStore.SetHintTechnique(this.HintTechnique);
+            //2.2
+            _glyphLayout.Typeface = this.Typeface;
+            _glyphLayout.ScriptLang = this.ScriptLang;
+            _glyphLayout.PositionTechnique = this.PositionTechnique;
+            _glyphLayout.EnableLigature = this.EnableLigature;
+            //3.
+            //color...
+        }
+
+        public void DrawString(RenderVxFormattedString renderVx, double x, double y)
+        {
+            float ox = canvasPainter.OriginX;
+            float oy = canvasPainter.OriginY;
+
+            //1. update some props.. 
+            //2. update current type face
+            UpdateTypefaceAndGlyphBuilder();
+            _glyphMeshStore.SetFont(_currentTypeface, this.FontSizeInPoints);
+            //3. layout glyphs with selected layout technique
+            //TODO: review this again, we should use pixel? 
+            float fontSizePoint = this.FontSizeInPoints;
+            float scale = _currentTypeface.CalculateToPixelScaleFromPointSize(fontSizePoint);
+            RenderVxGlyphPlan[] glyphPlans = renderVx.glyphList;
+            int j = glyphPlans.Length;
+            //---------------------------------------------------
+            //consider use cached glyph, to increase performance 
+
+            //GlyphPosPixelSnapKind x_snap = this.GlyphPosPixelSnapX;
+            //GlyphPosPixelSnapKind y_snap = this.GlyphPosPixelSnapY;
+            float g_x = 0;
+            float g_y = 0;
+            float baseY = (int)y;
+
+            for (int i = 0; i < j; ++i)
+            {
+                RenderVxGlyphPlan glyphPlan = glyphPlans[i];
+                //-----------------------------------
+                //TODO: review here ***
+                //PERFORMANCE revisit here 
+                //if we have create a vxs we can cache it for later use?
+                //-----------------------------------  
+                VertexStore vxs = _glyphMeshStore.GetGlyphMesh(glyphPlan.glyphIndex);
+                g_x = (float)(glyphPlan.x * scale + x);
+                g_y = (float)glyphPlan.y * scale;
+
+                canvasPainter.SetOrigin(g_x, g_y);
+                canvasPainter.Fill(vxs);
+            }
+            //restore prev origin
+            canvasPainter.SetOrigin(ox, oy);
+        }
+        public override void DrawFromGlyphPlans(List<GlyphPlan> glyphPlanList, int startAt, int len, float x, float y)
+        {
+            CanvasPainter canvasPainter = this.TargetCanvasPainter;
+            //Typeface typeface = _glyphPathBuilder.Typeface;
+            //3. layout glyphs with selected layout technique
+            //TODO: review this again, we should use pixel?
+
+            float fontSizePoint = this.FontSizeInPoints;
+            float scale = _currentTypeface.CalculateToPixelScaleFromPointSize(fontSizePoint);
+
+
+            //4. render each glyph
+            float ox = canvasPainter.OriginX;
+            float oy = canvasPainter.OriginY;
+            int endBefore = startAt + len;
+
+            //---------------------------------------------------
+            //consider use cached glyph, to increase performance 
+            _glyphMeshStore.SetFont(_currentTypeface, fontSizePoint);
+            //_hintGlyphCollection.SetCacheInfo(typeface, fontSizePoint, this.HintTechnique);
+            //---------------------------------------------------
+
+            float g_x = 0;
+            float g_y = 0;
+            float baseY = (int)y;
+            for (int i = startAt; i < endBefore; ++i)
+            {
+                GlyphPlan glyphPlan = glyphPlanList[i];
+                //-----------------------------------
+                //TODO: review here ***
+                //PERFORMANCE revisit here 
+                //if we have create a vxs we can cache it for later use?
+                //-----------------------------------  
+                VertexStore vxs = _glyphMeshStore.GetGlyphMesh(glyphPlan.glyphIndex);
+                g_x = glyphPlan.ExactX + x;
+                g_y = glyphPlan.ExactY;
+
+                canvasPainter.SetOrigin(g_x, g_y);
+                canvasPainter.Fill(vxs);
+            }
+            //restore prev origin
+            canvasPainter.SetOrigin(ox, oy);
+        }
+
+        public void DrawString(char[] text, int startAt, int len, double x, double y)
+        {
+            UpdateGlyphLayoutSettings();
+            _outputGlyphPlans.Clear();
+
+            //
+            float pxscale = _currentTypeface.CalculateToPixelScaleFromPointSize(this.FontSizeInPoints);
+            _glyphLayout.GenerateGlyphPlans(text, startAt, len, _outputGlyphPlans, null);
+            //-----
+            //we (fine) adjust horizontal fit here
+
+            //-----
+            DrawFromGlyphPlans(_outputGlyphPlans, (float)x, (float)y);
+        }
+        public override void DrawString(char[] textBuffer, int startAt, int len, float x, float y)
+        {
+            UpdateGlyphLayoutSettings();
+            _outputGlyphPlans.Clear();
+            //             
+            _glyphLayout.FontSizeInPoints = this.FontSizeInPoints;
+            _glyphLayout.GenerateGlyphPlans(textBuffer, startAt, len, _outputGlyphPlans, null);
+
+            //-----
+            //we (fine) adjust horizontal fit here
+            //this step we need grid fitting information
+
+            //-----
+            DrawFromGlyphPlans(_outputGlyphPlans, x, y);
+        }
+
+    }
+
+    public static class TextPrinterHelper
+    {
+        public static void CopyGlyphPlans(RenderVxFormattedString renderVx, List<GlyphPlan> glyphPlans, float scale)
+        {
+            int n = glyphPlans.Count;
+            //copy 
+            var renderVxGlyphPlans = new RenderVxGlyphPlan[n];
+            for (int i = 0; i < n; ++i)
+            {
+                GlyphPlan glyphPlan = glyphPlans[i];
+                renderVxGlyphPlans[i] = new RenderVxGlyphPlan(
+                    glyphPlan.glyphIndex,
+                    glyphPlan.ExactX,
+                    glyphPlan.ExactY,
+                    glyphPlan.AdvanceX
+                    );
+            }
+            renderVx.glyphList = renderVxGlyphPlans;
+        }
+    }
+
+
+}
