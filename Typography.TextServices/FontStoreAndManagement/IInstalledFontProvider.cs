93dbec0c5e6973bc716eb23893679f49f26d5983
win <wintercoredev@gmail.com>
2017-12-10 21:47:34 +0700

    notes

--- Typography.TextServices/FontStoreAndManagement/IInstalledFontProvider.cs
+++ Typography.TextServices/FontStoreAndManagement/IInstalledFontProvider.cs
@@ -6,4 +6,35 @@ namespace Typography.TextServices
     {
         IEnumerable<string> GetInstalledFontIter();
     }
+
+
+    ///// <summary>
+    ///// provide installed font from Windows directory
+    ///// </summary>
+    //public class InstalledFontsProviderWin32 : IInstalledFontProvider
+    //{
+    //    public IEnumerable<string> GetInstalledFontIter()
+    //    {
+    //        //-------------------------------------------------
+    //        //TODO: review here, this is not platform depend
+    //        //-------------------------------------------------
+    //        //check if MAC or linux font folder too
+    //        //-------------------------------------------------
+    //        string[] localMachineFonts = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows NT\\CurrentVersion\\Fonts", false).GetValueNames();
+    //        // get parent of System folder to have Windows folder
+    //        DirectoryInfo dirWindowsFolder = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.System));
+    //        string strFontsFolder = Path.Combine(dirWindowsFolder.FullName, "Fonts");
+    //        RegistryKey regKey = Registry.LocalMachine.OpenSubKey("Software\\Microsoft\\Windows NT\\CurrentVersion\\Fonts");
+    //        //---------------------------------------- 
+    //        foreach (string winFontName in localMachineFonts)
+    //        {
+    //            string f = (string)regKey.GetValue(winFontName);
+    //            if (f.EndsWith(".ttf") || f.EndsWith(".otf"))
+    //            {
+    //                yield return Path.Combine(strFontsFolder, f);
+    //            }
+    //        }
+    //    }
+
+    //}
 }
