764cfbf5c5a6abf71d7d944d8ca86cd3f65fed50
win <wintercoredev@gmail.com>
2018-01-07 10:53:10 +0700

    679.1: 2018

--- Typography.TextBreak/Typography.TextBreak/BreakingEngine.cs
+++ Typography.TextBreak/Typography.TextBreak/BreakingEngine.cs
@@ -1,4 +1,4 @@
-﻿//MIT, 2016-2017, WinterDev
+﻿//MIT, 2016-2018, WinterDev
 // some code from icu-project
 // © 2016 and later: Unicode, Inc. and others.
 // License & terms of use: http://www.unicode.org/copyright.html#License
