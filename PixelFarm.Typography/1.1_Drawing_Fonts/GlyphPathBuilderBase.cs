08419094605892da736507bb034313c6bd311ddc
win <wintercoredev@gmail.com>
2018-05-15 16:35:59 +0700

    748.1: HintTechnique

--- PixelFarm.Typography/1.1_Drawing_Fonts/GlyphPathBuilderBase.cs
+++ PixelFarm.Typography/1.1_Drawing_Fonts/GlyphPathBuilderBase.cs
@@ -160,4 +160,23 @@ namespace Typography.Contours
             }
         }
     }
+    public enum HintTechnique : byte
+    {
+        /// <summary>
+        /// no hinting
+        /// </summary>
+        None,
+        /// <summary>
+        /// truetype instruction
+        /// </summary>
+        TrueTypeInstruction,
+        /// <summary>
+        /// truetype instruction vertical only
+        /// </summary>
+        TrueTypeInstruction_VerticalOnly,
+        /// <summary>
+        /// custom hint
+        /// </summary>
+        CustomAutoFit
+    }
 }
