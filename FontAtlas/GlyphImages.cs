57ae1b47acd8756f88e6c0e1639cab8a05812c55
win <wintercoredev@gmail.com>
2018-06-27 17:59:44 +0700

    807: CacheGlyph remove unused code

--- FontAtlas/GlyphImages.cs
+++ FontAtlas/GlyphImages.cs
@@ -59,11 +59,11 @@ namespace Typography.Rendering
     {
         public int borderX;
         public int borderY;
-        public GlyphImage img;
+        internal GlyphImage img;
         public Rectangle area;
-        public char character;
+        public char character;//TODO: this should be code point(int32)
         public ushort glyphIndex;
-        public GlyphMatrix2 glyphMatrix;
+        
     }
     public class TextureGlyphMapData
     {
