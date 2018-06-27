54470936d50d2dba37296fdd9c15a49adbce55bd
win <wintercoredev@gmail.com>
2018-06-27 18:03:14 +0700

    808: remove unused GlyphMatrix2

--- FontAtlas/GlyphImages.cs
+++ FontAtlas/GlyphImages.cs
@@ -63,7 +63,7 @@ namespace Typography.Rendering
         public Rectangle area;
         public char character;//TODO: this should be code point(int32)
         public ushort glyphIndex;
-        
+
     }
     public class TextureGlyphMapData
     {
@@ -79,29 +79,4 @@ namespace Typography.Rendering
         public float TextureYOffset { get; set; }
     }
 
-    public struct GlyphMatrix2
-    {
-        public short unit_per_em;
-        public short ascender;
-        public short descender;
-        public short height;
-        public int advanceX;
-        public int advanceY;
-        public int bboxXmin;
-        public int bboxXmax;
-        public int bboxYmin;
-        public int bboxYmax;
-        public int img_width;
-        public int img_height;
-        public int img_horiBearingX;
-        public int img_horiBearingY;
-        public int img_horiAdvance;
-        public int img_vertBearingX;
-        public int img_vertBearingY;
-        public int img_vertAdvance;
-        public int bitmap_left;
-        public int bitmap_top;
-        //public IntPtr bitmap;
-        //public IntPtr outline;
-    }
 }
