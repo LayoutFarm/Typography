f3b67da55bdf22f74f03602988f3de408d450ccc
win <wintercoredev@gmail.com>
2018-06-28 14:55:54 +0700

    829: FontAtlasTextPrinter

---
+++ PixelFarm.Typography/3_FontFaceImpl/BitmapFontMx.cs
@@ -0,0 +1,294 @@
+﻿//MIT, 2016-present, WinterDev
+//----------------------------------- 
+using System;
+using System.Collections.Generic;
+using PixelFarm;
+using PixelFarm.Drawing;
+using PixelFarm.Drawing.Fonts;
+
+using Typography.OpenFont;
+using Typography.OpenFont.Extensions;
+
+using PixelFarm.Platforms;
+
+namespace Typography.Rendering
+{
+
+
+    public delegate U LoadNewBmpDelegate<T, U>(T src);
+
+    public class FontBitmapCache<T, U> : IDisposable
+        where U : IDisposable
+    {
+        Dictionary<T, U> _loadBmps = new Dictionary<T, U>();
+        LoadNewBmpDelegate<T, U> _loadNewBmpDel;
+        public FontBitmapCache(LoadNewBmpDelegate<T, U> loadNewBmpDel)
+        {
+            _loadNewBmpDel = loadNewBmpDel;
+        }
+        public U GetOrCreateNewOne(T key)
+        {
+            U found;
+            if (!_loadBmps.TryGetValue(key, out found))
+            {
+                return _loadBmps[key] = _loadNewBmpDel(key);
+            }
+            return found;
+        }
+        public void Dispose()
+        {
+            Clear();
+        }
+        public void Clear()
+        {
+            foreach (U glbmp in _loadBmps.Values)
+            {
+                glbmp.Dispose();
+            }
+            _loadBmps.Clear();
+        }
+        public void Delete(T key)
+        {
+            U found;
+            if (_loadBmps.TryGetValue(key, out found))
+            {
+                found.Dispose();
+                _loadBmps.Remove(key);
+            }
+        }
+    }
+
+    public class BitmapFontManager<B>
+        where B : IDisposable
+    {
+        static FontBitmapCache<SimpleFontAtlas, B> _loadedGlyphs;
+        static Dictionary<int, SimpleFontAtlas> _createdAtlases = new Dictionary<int, SimpleFontAtlas>();
+
+        LayoutFarm.OpenFontTextService textServices;
+        ScriptLang[] _currentScriptLangs;
+        TextureKind _textureKind;
+        public BitmapFontManager(TextureKind textureKind,
+            LayoutFarm.OpenFontTextService textServices,
+            LoadNewBmpDelegate<SimpleFontAtlas, B> _createNewDel)
+            : this(textureKind, textServices)
+        {
+            //glyph cahce for specific atlas 
+            SetLoadNewBmpDel(_createNewDel);
+        }
+        public BitmapFontManager(TextureKind textureKind, LayoutFarm.OpenFontTextService textServices)
+        {
+            this.textServices = textServices;
+            _textureKind = textureKind;
+        }
+
+
+        protected void SetLoadNewBmpDel(LoadNewBmpDelegate<SimpleFontAtlas, B> _createNewDel)
+        {
+            _loadedGlyphs = new FontBitmapCache<SimpleFontAtlas, B>(_createNewDel);
+        }
+        GlyphTextureBuildDetail[] _textureBuildDetails;
+        public void SetCurrentScriptLangs(ScriptLang[] currentScriptLangs)
+        {
+            this._currentScriptLangs = currentScriptLangs;
+
+            //TODO: review here again,
+            //this is a fixed version for tahoma font
+            //temp fix here ...
+            _textureBuildDetails = new GlyphTextureBuildDetail[]
+            {
+                new GlyphTextureBuildDetail{ ScriptLang= ScriptLangs.Latin, DoFilter= false, HintTechnique = Typography.Contours.HintTechnique.TrueTypeInstruction_VerticalOnly },
+                new GlyphTextureBuildDetail{ OnlySelectedGlyphIndices=new char[]{ 'x', 'X', '7','k','K','Z','z','R','Y','%' },
+                    DoFilter = false ,  HintTechnique = Typography.Contours.HintTechnique.None},
+                new GlyphTextureBuildDetail{ ScriptLang= ScriptLangs.Thai, DoFilter= false, HintTechnique = Typography.Contours.HintTechnique.None},
+            };
+        }
+
+#if DEBUG
+        System.Diagnostics.Stopwatch _dbugStopWatch = new System.Diagnostics.Stopwatch();
+#endif 
+        /// <summary>
+        /// get from cache or create a new one
+        /// </summary>
+        /// <param name="reqFont"></param>
+        /// <returns></returns>
+        public SimpleFontAtlas GetFontAtlas(RequestFont reqFont, out B outputBitmap)
+        {
+
+#if DEBUG
+            _dbugStopWatch.Reset();
+            _dbugStopWatch.Start();
+#endif
+
+            int fontKey = reqFont.FontKey;
+            SimpleFontAtlas fontAtlas;
+            if (!_createdAtlases.TryGetValue(fontKey, out fontAtlas))
+            {
+                //check from pre-built cache (if availiable) 
+                Typeface resolvedTypeface = textServices.ResolveTypeface(reqFont);
+
+                string fontTextureFile = reqFont.Name + " " + fontKey;
+                string resolveFontFile = fontTextureFile + ".info";
+                string fontTextureInfoFile = resolveFontFile;
+                string fontTextureImg = fontTextureInfoFile + ".png";
+
+                //check if the file exist
+
+                if (StorageService.Provider.DataExists(fontTextureInfoFile))
+                {
+                    SimpleFontAtlasBuilder atlasBuilder2 = new SimpleFontAtlasBuilder();
+
+                    using (System.IO.Stream dataStream = StorageService.Provider.ReadDataStream(fontTextureInfoFile))
+                    {
+                        try
+                        {
+                            fontAtlas = atlasBuilder2.LoadFontInfo(dataStream);
+                            fontAtlas.TotalGlyph = ReadGlyphImages(fontTextureImg);
+                            fontAtlas.OriginalFontSizePts = reqFont.SizeInPoints;
+                            _createdAtlases.Add(fontKey, fontAtlas);
+                            ////
+                            ////calculate some commonly used values
+                            //fontAtlas.SetTextureScaleInfo(
+                            //    resolvedTypeface.CalculateScaleToPixelFromPointSize(fontAtlas.OriginalFontSizePts),
+                            //    resolvedTypeface.CalculateScaleToPixelFromPointSize(reqFont.SizeInPoints));
+                            ////TODO: review here, use scaled or unscaled values
+                            //fontAtlas.SetCommonFontMetricValues(
+                            //    resolvedTypeface.Ascender,
+                            //    resolvedTypeface.Descender,
+                            //    resolvedTypeface.LineGap,
+                            //    resolvedTypeface.CalculateRecommendLineSpacing());
+                        }
+                        catch (Exception ex)
+                        {
+                            throw ex;
+                        }
+                    }
+
+                }
+                else
+                {
+
+                    GlyphImage totalGlyphsImg = null;
+                    SimpleFontAtlasBuilder atlasBuilder = null;
+                    var textureGen = new GlyphTextureBitmapGenerator();
+                    textureGen.CreateTextureFontFromScriptLangs(
+                        resolvedTypeface,
+                        reqFont.SizeInPoints,
+                       _textureKind,
+                       _textureBuildDetails,
+                        (glyphIndex, glyphImage, outputAtlasBuilder) =>
+                        {
+                            if (outputAtlasBuilder != null)
+                            {
+                                //finish
+                                atlasBuilder = outputAtlasBuilder;
+                            }
+                        }
+                    );
+
+                    atlasBuilder.SpaceCompactOption = SimpleFontAtlasBuilder.CompactOption.ArrangeByHeight;
+                    totalGlyphsImg = atlasBuilder.BuildSingleImage();
+                    //if (reqFont.SizeInPoints == 14 && cacheImg != null)
+                    //{
+                    //    totalGlyphsImg = cacheImg;
+                    //}
+                    //totalGlyphsImg = Sharpen(totalGlyphsImg, 1); //test shapen primary image
+                    //-               
+                    //
+                    //create atlas
+                    fontAtlas = atlasBuilder.CreateSimpleFontAtlas();
+                    fontAtlas.TotalGlyph = totalGlyphsImg;
+#if DEBUG
+                    //save glyph image for debug
+                    //PixelFarm.Agg.ActualImage.SaveImgBufferToPngFile(
+                    //    totalGlyphsImg.GetImageBuffer(),
+                    //    totalGlyphsImg.Width * 4,
+                    //    totalGlyphsImg.Width, totalGlyphsImg.Height,
+                    //    "d:\\WImageTest\\total_" + reqFont.Name + "_" + reqFont.SizeInPoints + ".png");
+                    ////save image to cache
+                    SaveImgBufferToFile(totalGlyphsImg, fontTextureImg);
+#endif
+
+                    //cache the atlas
+                    _createdAtlases.Add(fontKey, fontAtlas);
+                    //
+                    ////calculate some commonly used values
+                    //fontAtlas.SetTextureScaleInfo(
+                    //    resolvedTypeface.CalculateScaleToPixelFromPointSize(fontAtlas.OriginalFontSizePts),
+                    //    resolvedTypeface.CalculateScaleToPixelFromPointSize(reqFont.SizeInPoints));
+                    ////TODO: review here, use scaled or unscaled values
+                    //fontAtlas.SetCommonFontMetricValues(
+                    //    resolvedTypeface.Ascender,
+                    //    resolvedTypeface.Descender,
+                    //    resolvedTypeface.LineGap,
+                    //    resolvedTypeface.CalculateRecommendLineSpacing());
+
+                    ///
+#if DEBUG
+
+                    _dbugStopWatch.Stop();
+                    System.Diagnostics.Debug.WriteLine("build font atlas: " + _dbugStopWatch.ElapsedMilliseconds + " ms");
+#endif
+
+                    //save font info to cache
+                    using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+                    {
+                        atlasBuilder.SaveFontInfo(ms);
+                        System.IO.File.WriteAllBytes(fontTextureInfoFile, ms.ToArray());
+
+                        StorageService.Provider.SaveData(fontTextureInfoFile, ms.ToArray());
+                    }
+                }
+            }
+
+            outputBitmap = _loadedGlyphs.GetOrCreateNewOne(fontAtlas);
+            return fontAtlas;
+        }
+
+        static GlyphImage ReadGlyphImages(string filename)
+        {
+            PixelFarm.CpuBlit.ActualBitmap bmp = StorageService.Provider.ReadPngBitmap(filename);
+            GlyphImage img = new GlyphImage(bmp.Width, bmp.Height);
+            img.SetImageBuffer(PixelFarm.CpuBlit.ActualBitmap.GetBuffer(bmp), true);
+            return img;
+
+        }
+        static void SaveImgBufferToFile(GlyphImage glyphImg, string filename)
+        {
+
+            var bmp = new PixelFarm.CpuBlit.ActualBitmap(glyphImg.Width, glyphImg.Height, glyphImg.GetImageBuffer());
+            StorageService.Provider.SavePngBitmap(bmp, filename);
+
+        }
+#if DEBUG
+        /// <summary>
+        /// test only, shapen org image with Paint.net sharpen filter
+        /// </summary>
+        /// <param name="org"></param>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        static GlyphImage Sharpen(GlyphImage org, int radius)
+        {
+
+            GlyphImage newImg = new GlyphImage(org.Width, org.Height);
+
+            PixelFarm.CpuBlit.Imaging.ShapenFilterPdn sharpen1 = new PixelFarm.CpuBlit.Imaging.ShapenFilterPdn();
+            int[] orgBuffer = org.GetImageBuffer();
+            unsafe
+            {
+                fixed (int* orgHeader = &orgBuffer[0])
+                {
+                    int[] output = sharpen1.Sharpen(orgHeader, org.Width, org.Height, org.Width * 4, radius);
+                    newImg.SetImageBuffer(output, org.IsBigEndian);
+                }
+            }
+
+            return newImg;
+        }
+#endif
+        public void Clear()
+        {
+            _loadedGlyphs.Clear();
+        }
+    }
+
+}
---
+++ PixelFarm.Typography/3_FontFaceImpl/GlyphTextureBitmapGenerator.cs
@@ -0,0 +1,226 @@
+﻿//MIT, 2017-present, WinterDev
+using System;
+using System.Collections.Generic;
+
+
+
+using Typography.OpenFont;
+using Typography.TextLayout;
+using Typography.Contours;
+using Typography.Rendering;
+
+namespace PixelFarm.Drawing.Fonts
+{
+    public class GlyphTextureBuildDetail
+    {
+        public ScriptLang ScriptLang;
+        public char[] OnlySelectedGlyphIndices;
+        public HintTechnique HintTechnique;
+        public bool DoFilter;
+    }
+
+    public class GlyphTextureBitmapGenerator
+    {
+
+        public delegate void OnEachFinishTotal(int glyphIndex, GlyphImage glyphImage, SimpleFontAtlasBuilder atlasBuilder);
+
+
+        public GlyphTextureBitmapGenerator()
+        {
+
+
+        }
+        public void CreateTextureFontFromScriptLangs(
+            Typeface typeface, float sizeInPoint,
+            TextureKind textureKind,
+            GlyphTextureBuildDetail[] details,
+            OnEachFinishTotal onFinishTotal)
+        {
+            //-------------------------------------------------------------
+            var atlasBuilder = new SimpleFontAtlasBuilder();
+            atlasBuilder.SetAtlasInfo(textureKind, sizeInPoint);
+            //-------------------------------------------------------------  
+            int j = details.Length;
+            for (int i = 0; i < j; ++i)
+            {
+                GlyphTextureBuildDetail detail = details[i];
+                if (detail.ScriptLang != null)
+                {
+                    //skip those script lang=null
+                    //2. find associated glyph index base on input script langs
+                    List<ushort> outputGlyphIndexList = new List<ushort>();
+                    typeface.CollectAllAssociateGlyphIndex(outputGlyphIndexList, detail.ScriptLang);
+                    CreateTextureFontFromGlyphIndices(typeface,
+                        sizeInPoint,
+                        detail.HintTechnique,
+                        atlasBuilder,
+                        detail.DoFilter,
+                        GetUniqueGlyphIndexList(outputGlyphIndexList)
+                        );
+                }
+            }
+            for (int i = 0; i < j; ++i)
+            {
+                GlyphTextureBuildDetail detail = details[i];
+                if (detail.OnlySelectedGlyphIndices != null)
+                {
+                    //skip those script lang=null
+                    //2. find associated glyph index base on input script langs
+
+                    CreateTextureFontFromGlyphIndices(typeface,
+                        sizeInPoint,
+                        detail.HintTechnique,
+                        atlasBuilder,
+                        detail.DoFilter,
+                        detail.OnlySelectedGlyphIndices
+                        );
+                }
+            }
+            onFinishTotal(0, null, atlasBuilder);
+        }
+
+        public void CreateTextureFontFromInputChars(
+            Typeface typeface, float sizeInPoint,
+            TextureKind textureKind,
+            char[] chars,
+            OnEachFinishTotal onFinishTotal)
+        {
+
+            //convert input chars into glyphIndex
+            List<ushort> glyphIndices = new List<ushort>(chars.Length);
+            int i = 0;
+            foreach (char ch in chars)
+            {
+                glyphIndices.Add(typeface.LookupIndex(ch));
+                i++;
+            }
+            //-------------------------------------------------------------
+            var atlasBuilder = new SimpleFontAtlasBuilder();
+            atlasBuilder.SetAtlasInfo(textureKind, sizeInPoint);
+            //------------------------------------------------------------- 
+            //we can specfic subset with special setting for each set 
+            CreateTextureFontFromGlyphIndices(typeface, sizeInPoint,
+                HintTechnique.TrueTypeInstruction_VerticalOnly, atlasBuilder, false, GetUniqueGlyphIndexList(glyphIndices));
+            onFinishTotal(0, null, atlasBuilder);
+        }
+        void CreateTextureFontFromGlyphIndices(
+              Typeface typeface,
+              float sizeInPoint,
+              HintTechnique hintTechnique,
+              SimpleFontAtlasBuilder atlasBuilder,
+              bool applyFilter,
+              char[] chars)
+        {
+            int j = chars.Length;
+            ushort[] glyphIndices = new ushort[j];
+            for (int i = 0; i < j; ++i)
+            {
+                glyphIndices[i] = typeface.LookupIndex(chars[i]);
+            }
+
+            CreateTextureFontFromGlyphIndices(typeface, sizeInPoint, hintTechnique, atlasBuilder, applyFilter, glyphIndices);
+        }
+        void CreateTextureFontFromGlyphIndices(
+              Typeface typeface,
+              float sizeInPoint,
+              HintTechnique hintTechnique,
+              SimpleFontAtlasBuilder atlasBuilder,
+              bool applyFilter,
+              ushort[] glyphIndices)
+        {
+
+            //sample: create sample msdf texture 
+            //-------------------------------------------------------------
+            var builder = new GlyphPathBuilder(typeface);
+            builder.SetHintTechnique(hintTechnique);
+            //
+            if (atlasBuilder.TextureKind == TextureKind.Msdf)
+            {
+                MsdfGenParams msdfGenParams = new MsdfGenParams();
+                int j = glyphIndices.Length;
+                for (int i = 0; i < j; ++i)
+                {
+                    ushort gindex = glyphIndices[i];
+                    //create picture with unscaled version set scale=-1
+                    //(we will create glyph contours and analyze them)
+                    builder.BuildFromGlyphIndex(gindex, -1);
+                    var glyphToContour = new GlyphContourBuilder();
+                    builder.ReadShapes(glyphToContour);
+                    //msdfgen with  scale the glyph to specific shapescale
+                    msdfGenParams.shapeScale = 1f / 64; //as original
+                    GlyphImage glyphImg = MsdfGlyphGen.CreateMsdfImage(glyphToContour, msdfGenParams);
+                    //
+                    atlasBuilder.AddGlyph(gindex, glyphImg);
+                }
+            }
+            else
+            {
+                AggGlyphTextureGen aggTextureGen = new AggGlyphTextureGen();
+                aggTextureGen.TextureKind = atlasBuilder.TextureKind;
+                int j = glyphIndices.Length;
+                for (int i = 0; i < j; ++i)
+                {
+                    //build glyph
+                    ushort gindex = glyphIndices[i];
+                    builder.BuildFromGlyphIndex(gindex, sizeInPoint);
+
+                    GlyphImage glyphImg = aggTextureGen.CreateGlyphImage(builder, 1);
+                    if (applyFilter)
+                    {
+
+                        glyphImg = Sharpen(glyphImg, 1);
+                        //TODO:
+                        //the filter make the image shift x and y 1 px
+                        //temp fix with this,
+                        glyphImg.TextureOffsetX += 1;
+                        glyphImg.TextureOffsetY += 1;
+                    }
+                    //
+                    atlasBuilder.AddGlyph(gindex, glyphImg);
+                }
+            }
+        }
+        /// <summary>
+        /// test only, shapen org image with Paint.net sharpen filter
+        /// </summary>
+        /// <param name="org"></param>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        static GlyphImage Sharpen(GlyphImage org, int radius)
+        {
+            GlyphImage newImg = new GlyphImage(org.Width, org.Height);
+            CpuBlit.Imaging.ShapenFilterPdn sharpen1 = new CpuBlit.Imaging.ShapenFilterPdn();
+            int[] orgBuffer = org.GetImageBuffer();
+            unsafe
+            {
+                fixed (int* orgHeader = &orgBuffer[0])
+                {
+                    int[] output = sharpen1.Sharpen(orgHeader, org.Width, org.Height, org.Width * 4, radius);
+                    newImg.SetImageBuffer(output, org.IsBigEndian);
+                }
+            }
+
+            return newImg;
+        }
+        static ushort[] GetUniqueGlyphIndexList(List<ushort> inputGlyphIndexList)
+        {
+            Dictionary<ushort, bool> uniqueGlyphIndices = new Dictionary<ushort, bool>(inputGlyphIndexList.Count);
+            foreach (ushort glyphIndex in inputGlyphIndexList)
+            {
+                if (!uniqueGlyphIndices.ContainsKey(glyphIndex))
+                {
+                    uniqueGlyphIndices.Add(glyphIndex, true);
+                }
+            }
+            //
+            ushort[] uniqueGlyphIndexArray = new ushort[uniqueGlyphIndices.Count];
+            int i = 0;
+            foreach (ushort glyphIndex in uniqueGlyphIndices.Keys)
+            {
+                uniqueGlyphIndexArray[i] = glyphIndex;
+                i++;
+            }
+            return uniqueGlyphIndexArray;
+        }
+    }
+}
