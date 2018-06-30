e7a75f37e89ef718bee0d911e39f079ea89d004b
win <wintercoredev@gmail.com>
2018-06-30 23:36:19 +0700

    666: update iOS and Android example
    use shared project

--- Demo/Shared/DrawingGL.Text/TextPrinter.cs
+++ Demo/Shared/DrawingGL.Text/TextPrinter.cs
@@ -73,13 +73,10 @@ namespace DrawingGL.Text
         }
         public MeasuredStringBox Measure(char[] textBuffer, int startAt, int len)
         {
-
-            return SampleMeasureStringUtil.MeasureString(
-                this.GlyphLayoutMan,
-                this.FontSizeInPoints,
+            return GlyphLayoutMan.LayoutAndMeasureString(
                 textBuffer, startAt, len,
-                out int w,
-                out int h);
+                this.FontSizeInPoints
+                );
         }
 
         /// <summary>
@@ -210,69 +207,5 @@ namespace DrawingGL.Text
         }
     }
 
-    public static class SampleMeasureStringUtil
-    {
-        //-----------------
-        //measure string utils
 
-        static PxScaledGlyphPlanList _reusableScaledGlyphPlanList = new PxScaledGlyphPlanList();
-        static List<MeasuredStringBox> _reusableMeasureBoxList = new List<MeasuredStringBox>();
-
-        static UnscaledGlyphPlanList _reusableGlyphPlanList = new UnscaledGlyphPlanList();
-
-        public static MeasuredStringBox MeasureString(
-             Typography.TextLayout.GlyphLayout glyphLayout,
-             float fontSizeInPts,
-             char[] str, int startAt, int len, out int w, out int h)
-        {
-            //measure string 
-            //check if we use cache feature or not
-
-            Typography.OpenFont.Typeface typeface = glyphLayout.Typeface;
-
-            if (str.Length < 1)
-            {
-                w = h = 0;
-            }
-            _reusableMeasureBoxList.Clear(); //reset 
-
-            float pxscale = typeface.CalculateScaleToPixelFromPointSize(fontSizeInPts);
-            //NOET:at this moment, simple operation
-            //may not be simple...  
-            //-------------------
-            //input string may contain more than 1 script lang
-            //user can parse it by other parser
-            //but in this code, we use our Typography' parser
-            //-------------------
-            //user must setup the CustomBreakerBuilder before use         
-
-            int cur_startAt = startAt;
-            float accumW = 0;
-            float accumH = 0;
-
-
-
-            glyphLayout.Layout(str, 0, str.Length);
-            //
-            _reusableGlyphPlanList.Clear();
-            GlyphLayoutExtensions.GenerateGlyphPlans(
-                glyphLayout.ResultUnscaledGlyphPositions,
-                pxscale,
-                true,
-                _reusableScaledGlyphPlanList);
-            //measure string size
-            var result = new MeasuredStringBox(
-                _reusableGlyphPlanList.AccumAdvanceX,
-                typeface.Ascender * pxscale,
-                typeface.Descender * pxscale,
-                typeface.LineGap * pxscale,
-                 Typography.OpenFont.Extensions.TypefaceExtensions.CalculateRecommendLineSpacing(typeface) * pxscale);
-
-
-            w = (int)System.Math.Round(accumW);
-            h = (int)System.Math.Round(accumH);
-
-            return result;
-        }
-    }
 }
