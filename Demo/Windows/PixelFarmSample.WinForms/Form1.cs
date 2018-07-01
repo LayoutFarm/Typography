d438febec2416d8af7e35bf504b36cc7e90e2490
win <wintercoredev@gmail.com>
2018-07-01 17:34:28 +0700

    674: update missing code

--- Demo/Windows/PixelFarmSample.WinForms/Form1.cs
+++ Demo/Windows/PixelFarmSample.WinForms/Form1.cs
@@ -434,7 +434,7 @@ namespace SampleWinForms
                 sampleFontFile,
                 18,
                 0,
-                255,
+                100,
                 "d:\\WImageTest\\sample_msdf.png");
             //---------------------------------------------------------
             //2. for debug, create from some unicode chars
@@ -566,17 +566,19 @@ namespace SampleWinForms
                     //glyphToContour.Read(builder.GetOutputPoints(), builder.GetOutputContours());
                     var genParams = new MsdfGenParams();
                     builder.ReadShapes(glyphToContour);
-                    genParams.shapeScale = 1f / 64; //we scale later (as original C++ code use 1/64)
+                    //genParams.shapeScale = 1f / 64; //we scale later (as original C++ code use 1/64)
                     GlyphImage glyphImg = MsdfGlyphGen.CreateMsdfImage(glyphToContour, genParams);
                     atlasBuilder.AddGlyph(gindex, glyphImg);
 
-                    //using (Bitmap bmp = new Bitmap(w, h, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
-                    //{
-                    //    var bmpdata = bmp.LockBits(new Rectangle(0, 0, w, h), System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
-                    //    System.Runtime.InteropServices.Marshal.Copy(buffer, 0, bmpdata.Scan0, buffer.Length);
-                    //    bmp.UnlockBits(bmpdata);
-                    //    bmp.Save("d:\\WImageTest\\a001_xn2_" + n + ".png");
-                    //}
+                    using (Bitmap bmp = new Bitmap(glyphImg.Width, glyphImg.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb))
+                    {
+                        int[] buffer = glyphImg.GetImageBuffer();
+                         
+                        var bmpdata = bmp.LockBits(new System.Drawing.Rectangle(0, 0, glyphImg.Width, glyphImg.Height), System.Drawing.Imaging.ImageLockMode.ReadWrite, bmp.PixelFormat);
+                        System.Runtime.InteropServices.Marshal.Copy(buffer, 0, bmpdata.Scan0, buffer.Length); 
+                        bmp.UnlockBits(bmpdata); 
+                        bmp.Save("d:\\WImageTest\\a001_xn2_" + gindex + ".png");
+                    }
                 }
 
                 var glyphImg2 = atlasBuilder.BuildSingleImage();
@@ -590,7 +592,12 @@ namespace SampleWinForms
                     bmp.UnlockBits(bmpdata);
                     bmp.Save("d:\\WImageTest\\a_total.png");
                 }
-                atlasBuilder.SaveFontInfo("d:\\WImageTest\\a_info.xml");
+                atlasBuilder.SaveFontInfo("d:\\WImageTest\\a_info.bin");
+                //
+                //-----------
+                //test read texture info back
+                var atlasBuilder2 = new SimpleFontAtlasBuilder();
+                var readbackFontAtlas = atlasBuilder2.LoadFontInfo("d:\\WImageTest\\a_info.bin");
             }
         }
         private void Form1_Load(object sender, EventArgs e)
