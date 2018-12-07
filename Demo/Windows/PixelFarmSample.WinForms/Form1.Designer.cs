aef3be18fda1b03ac87ba4ee07d325ba66a75d3c
win <wintercoredev@gmail.com>
2018-12-08 06:38:22 +0700

    769: add IcuSimpleTextFileDictionaryProvider

--- Demo/Windows/PixelFarmSample.WinForms/Form1.Designer.cs
+++ Demo/Windows/PixelFarmSample.WinForms/Form1.Designer.cs
@@ -156,7 +156,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+           
             this.ResumeLayout(false);
             this.PerformLayout();
 
