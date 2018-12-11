03dd3bc048888f8cb7c11b25e312806b85d129a2
win <wintercoredev@gmail.com>
2018-12-11 11:27:27 +0700

    789: MeasuredStringBox

--- Typography.GlyphLayout/MeasureStringBox.cs
+++ Typography.GlyphLayout/MeasureStringBox.cs
@@ -3,29 +3,52 @@ namespace Typography.TextLayout
 {
     public struct MeasuredStringBox
     {
+        /// <summary>
+        /// pixel scaled size
+        /// </summary>
+        public readonly float width; //4 
+        readonly float _pxscale; //4
 
-        public readonly float width;
-        public readonly float ascending;
-        public readonly float descending;
-        public readonly float lineGap;
-        public readonly float btbd;//Baseline-to-Baseline Distance
+        readonly short _ascending; //2
+        readonly short _descending;//2
+        readonly short _lineGap; //2
+        readonly short _btbd;//Baseline-to-Baseline Distance,  2 byte
 
-        public MeasuredStringBox(float width, float ascending, float descending, float lineGap, float btbd)
+        public ushort stopAt;
+
+        public MeasuredStringBox(float width,
+            short ascending,
+            short descending,
+            short lineGap,
+            short btbd,
+            float pxscale)
         {
             this.width = width;
-            this.ascending = ascending;
-            this.descending = descending;
-            this.lineGap = lineGap;
-            this.btbd = btbd;
+            this.stopAt = 0;
+            _ascending = ascending;
+            _descending = descending;
+            _lineGap = lineGap;
+            _btbd = btbd;
+            _pxscale = pxscale;
+
         }
+        public float ascending => _ascending * _pxscale;
+        public float descending => _descending * _pxscale;
+        public float lineGap => _lineGap * _pxscale;
+        public float btbd => _btbd * _pxscale;
+
         public static MeasuredStringBox operator *(MeasuredStringBox box, float scale)
         {
             //scale ***
-            return new MeasuredStringBox(box.width * scale,
-                box.ascending * scale,
-                box.descending * scale,
-                box.lineGap * scale,
-                box.btbd * scale);
+            var measureBox = new MeasuredStringBox(box.width * scale,
+                                box._ascending,
+                                box._descending,
+                                box._lineGap,
+                                box._btbd,
+                                box._pxscale * scale
+                                );
+            measureBox.stopAt = box.stopAt;
+            return measureBox;
         }
     }
 
