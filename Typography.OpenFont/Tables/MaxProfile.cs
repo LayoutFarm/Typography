92e362c736b031ce001b80508a5ec67466bedb1c
win <wintercoredev@gmail.com>
2019-08-13 08:59:20 +0700

    894: add maxp note

--- Typography.OpenFont/Tables/MaxProfile.cs
+++ Typography.OpenFont/Tables/MaxProfile.cs
@@ -1,14 +1,46 @@
 ﻿//Apache2, 2017-present, WinterDev
 //Apache2, 2014-2016, Samuel Carlsson, WinterDev
 
+
 using System.IO;
 namespace Typography.OpenFont.Tables
 {
+    //https://docs.microsoft.com/en-us/typography/opentype/spec/maxp
     class MaxProfile : TableEntry
     {
         public const string _N = "maxp";
         public override string Name => _N;
-        //s
+
+        //This table establishes the memory requirements for this font.
+        //Fonts with CFF data must use Version 0.5 of this table,
+        //specifying only the numGlyphs field.
+
+        //Fonts with TrueType outlines must use Version 1.0 of this table,  where all data is required.
+
+        //Version 0.5
+        //Type      Name        Description
+        //Fixed     version 	0x00005000 for version 0.5
+        //                      (Note the difference in the representation of a non-zero fractional part, in Fixed numbers.)
+        //uint16 numGlyphs      The number of glyphs in the font.
+
+        //Version 1.0
+        //Type      Name                Description
+        //Fixed     version 	        0x00010000 for version 1.0.
+        //uint16    numGlyphs           The number of glyphs in the font.
+        //uint16    maxPoints           Maximum points in a non-composite glyph.
+        //uint16    maxContours         Maximum contours in a non-composite glyph.
+        //uint16    maxCompositePoints  Maximum points in a composite glyph.
+        //uint16    maxCompositeContours    Maximum contours in a composite glyph.
+        //uint16    maxZones 	        1 if instructions do not use the twilight zone (Z0), or 2 if instructions do use Z0; should be set to 2 in most cases.
+        //uint16    maxTwilightPoints   Maximum points used in Z0.
+        //uint16    maxStorage          Number of Storage Area locations.
+        //uint16    maxFunctionDefs     Number of FDEFs, equal to the highest function number + 1.
+        //uint16    maxInstructionDefs  Number of IDEFs.
+        //uint16    maxStackElements    Maximum stack depth across Font Program ('fpgm' table), CVT Program('prep' table) and all glyph instructions(in the 'glyf' table).
+        //uint16    maxSizeOfInstructions   Maximum byte count for glyph instructions.
+        //uint16    maxComponentElements    Maximum number of components referenced at “top level” for any composite glyph.
+        //uint16    maxComponentDepth       Maximum levels of recursion; 1 for simple components.
+
         public uint Version { get; private set; }
         public ushort GlyphCount { get; private set; }
         public ushort MaxPointsPerGlyph { get; private set; }
