3d067fc62d33e7cc3cbbe4df6536c9c7d26eb6da
win <wintercoredev@gmail.com>
2020-02-02 07:17:53 +0700

    947.1

--- Typography.OpenFont/Tables.CFF/CFF.cs
+++ Typography.OpenFont/Tables.CFF/CFF.cs
@@ -1,4 +1,4 @@
-﻿//Apapche2, 2018, apache/pdfbox Authors ( https://github.com/apache/pdfbox) 
+﻿//Apache2, 2018, apache/pdfbox Authors ( https://github.com/apache/pdfbox) 
 //
 //Apache PDFBox
 //Copyright 2014 The Apache Software Foundation
