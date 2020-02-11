7a6dfecbd0b792587a44a7cbe46f9a00fc5160bc
win <wintercoredev@gmail.com>
2020-02-11 20:53:39 +0700

    970: add Typography.One and an example
    only for NETSTD 2.0+

---
+++ Demo/Windows/GdiPlusSample.WinForms_2/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace SampleWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstFontList = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lstFontSizes = new System.Windows.Forms.ListBox();
+            this.txtInputChar = new System.Windows.Forms.TextBox();
+            this.chkFillBackground = new System.Windows.Forms.CheckBox();
+            this.chkBorder = new System.Windows.Forms.CheckBox();
+            this.cmbPositionTech = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lstHintList = new System.Windows.Forms.ListBox();
+            this.cmdMeasureTextSpan = new System.Windows.Forms.Button();
+            this.chkFlipY = new System.Windows.Forms.CheckBox();
+            this.txtMsgInfo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lstFontList
+            // 
+            this.lstFontList.FormattingEnabled = true;
+            this.lstFontList.Location = new System.Drawing.Point(693, 39);
+            this.lstFontList.Name = "lstFontList";
+            this.lstFontList.Size = new System.Drawing.Size(379, 121);
+            this.lstFontList.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(948, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Size in Points";
+            // 
+            // lstFontSizes
+            // 
+            this.lstFontSizes.FormattingEnabled = true;
+            this.lstFontSizes.Location = new System.Drawing.Point(951, 184);
+            this.lstFontSizes.Name = "lstFontSizes";
+            this.lstFontSizes.Size = new System.Drawing.Size(121, 212);
+            this.lstFontSizes.TabIndex = 27;
+            // 
+            // txtInputChar
+            // 
+            this.txtInputChar.Location = new System.Drawing.Point(693, 270);
+            this.txtInputChar.Name = "txtInputChar";
+            this.txtInputChar.Size = new System.Drawing.Size(101, 20);
+            this.txtInputChar.TabIndex = 26;
+            this.txtInputChar.Text = "tfjpb09";
+            // 
+            // chkFillBackground
+            // 
+            this.chkFillBackground.AutoSize = true;
+            this.chkFillBackground.Location = new System.Drawing.Point(756, 12);
+            this.chkFillBackground.Name = "chkFillBackground";
+            this.chkFillBackground.Size = new System.Drawing.Size(101, 17);
+            this.chkFillBackground.TabIndex = 31;
+            this.chkFillBackground.Text = "Fill BackGround";
+            this.chkFillBackground.UseVisualStyleBackColor = true;
+            // 
+            // chkBorder
+            // 
+            this.chkBorder.AutoSize = true;
+            this.chkBorder.Location = new System.Drawing.Point(693, 12);
+            this.chkBorder.Name = "chkBorder";
+            this.chkBorder.Size = new System.Drawing.Size(57, 17);
+            this.chkBorder.TabIndex = 30;
+            this.chkBorder.Text = "Border";
+            this.chkBorder.UseVisualStyleBackColor = true;
+            // 
+            // cmbPositionTech
+            // 
+            this.cmbPositionTech.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPositionTech.FormattingEnabled = true;
+            this.cmbPositionTech.Location = new System.Drawing.Point(693, 168);
+            this.cmbPositionTech.Name = "cmbPositionTech";
+            this.cmbPositionTech.Size = new System.Drawing.Size(224, 21);
+            this.cmbPositionTech.TabIndex = 32;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(693, 316);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 37);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "Render!";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lstHintList
+            // 
+            this.lstHintList.FormattingEnabled = true;
+            this.lstHintList.Location = new System.Drawing.Point(693, 195);
+            this.lstHintList.Name = "lstHintList";
+            this.lstHintList.Size = new System.Drawing.Size(224, 69);
+            this.lstHintList.TabIndex = 36;
+            // 
+            // cmdMeasureTextSpan
+            // 
+            this.cmdMeasureTextSpan.Location = new System.Drawing.Point(693, 359);
+            this.cmdMeasureTextSpan.Name = "cmdMeasureTextSpan";
+            this.cmdMeasureTextSpan.Size = new System.Drawing.Size(153, 37);
+            this.cmdMeasureTextSpan.TabIndex = 37;
+            this.cmdMeasureTextSpan.Text = "MeasureTextSpan";
+            this.cmdMeasureTextSpan.UseVisualStyleBackColor = true;
+            this.cmdMeasureTextSpan.Click += new System.EventHandler(this.cmdMeasureTextSpan_Click);
+            // 
+            // chkFlipY
+            // 
+            this.chkFlipY.AutoSize = true;
+            this.chkFlipY.Checked = true;
+            this.chkFlipY.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkFlipY.Location = new System.Drawing.Point(693, 403);
+            this.chkFlipY.Name = "chkFlipY";
+            this.chkFlipY.Size = new System.Drawing.Size(49, 17);
+            this.chkFlipY.TabIndex = 38;
+            this.chkFlipY.Text = "FlipY";
+            this.chkFlipY.UseVisualStyleBackColor = true;
+            this.chkFlipY.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // txtMsgInfo
+            // 
+            this.txtMsgInfo.Location = new System.Drawing.Point(693, 437);
+            this.txtMsgInfo.Multiline = true;
+            this.txtMsgInfo.Name = "txtMsgInfo";
+            this.txtMsgInfo.Size = new System.Drawing.Size(379, 178);
+            this.txtMsgInfo.TabIndex = 39;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1185, 750);
+            this.Controls.Add(this.txtMsgInfo);
+            this.Controls.Add(this.chkFlipY);
+            this.Controls.Add(this.cmdMeasureTextSpan);
+            this.Controls.Add(this.lstHintList);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cmbPositionTech);
+            this.Controls.Add(this.chkFillBackground);
+            this.Controls.Add(this.chkBorder);
+            this.Controls.Add(this.lstFontList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lstFontSizes);
+            this.Controls.Add(this.txtInputChar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstFontList;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lstFontSizes;
+        private System.Windows.Forms.TextBox txtInputChar;
+        private System.Windows.Forms.CheckBox chkFillBackground;
+        private System.Windows.Forms.CheckBox chkBorder;
+        private System.Windows.Forms.ComboBox cmbPositionTech;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lstHintList;
+        private System.Windows.Forms.Button cmdMeasureTextSpan;
+        private System.Windows.Forms.CheckBox chkFlipY;
+        private System.Windows.Forms.TextBox txtMsgInfo;
+    }
+}
---
+++ Demo/Windows/GdiPlusSample.WinForms_2/Form1.cs
@@ -0,0 +1,325 @@
+﻿//MIT, 2016-present, WinterDev
+using System.IO;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+// 
+using Typography.TextLayout;
+using Typography.Contours;
+using Typography.TextServices;
+using Typography.OpenFont;
+using Typography.OpenFont.Extensions;
+namespace SampleWinForms
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        //for this sample code,
+        //create text printer env for developer.
+        DevGdiTextPrinter _currentTextPrinter = new DevGdiTextPrinter();
+        InstalledFontCollection installedFontCollection;
+        TypefaceStore _typefaceStore;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //choose Thai script for 'complex script' testing.
+            //you can change this to test other script.
+            _currentTextPrinter.ScriptLang = Typography.OpenFont.ScriptLangs.Thai;
+            //----------
+            button1.Click += (s, e) => UpdateRenderOutput();
+            //simple load test fonts from local test dir
+            //and send it into test list
+            chkFillBackground.Checked = true;
+            chkBorder.CheckedChanged += (s, e) => UpdateRenderOutput();
+            chkFillBackground.CheckedChanged += (s, e) => UpdateRenderOutput();
+            //----------
+            cmbPositionTech.Items.Add(PositionTechnique.OpenFont);
+            cmbPositionTech.Items.Add(PositionTechnique.Kerning);
+            cmbPositionTech.Items.Add(PositionTechnique.None);
+            cmbPositionTech.SelectedIndex = 0;
+            cmbPositionTech.SelectedIndexChanged += (s, e) => UpdateRenderOutput();
+            //----------
+            lstHintList.Items.Add(HintTechnique.None);
+            lstHintList.Items.Add(HintTechnique.TrueTypeInstruction);
+            lstHintList.Items.Add(HintTechnique.TrueTypeInstruction_VerticalOnly);
+            //lstHintList.Items.Add(HintTechnique.CustomAutoFit);
+            lstHintList.SelectedIndex = 0;
+            lstHintList.SelectedIndexChanged += (s, e) => UpdateRenderOutput();
+            //---------- 
+            txtInputChar.TextChanged += (s, e) => UpdateRenderOutput();
+            //
+
+            //1. create font collection             
+            installedFontCollection = new InstalledFontCollection();
+            //2. set some essential handler
+            installedFontCollection.SetFontNameDuplicatedHandler((f1, f2) => FontNameDuplicatedDecision.Skip);
+
+            installedFontCollection.LoadFontsFromFolder("../../../TestFonts_Err");
+            installedFontCollection.LoadFontsFromFolder("../../../TestFonts");
+            //installedFontCollection.LoadSystemFonts();
+
+            //---------- 
+            //show result
+            InstalledFont selectedFF = null;
+            int selected_index = 0;
+            int ffcount = 0;
+            bool found = false;
+            foreach (InstalledFont ff in installedFontCollection.GetInstalledFontIter())
+            {
+                if (!found && ff.FontName == "Source Sans Pro")
+                {
+                    selectedFF = ff;
+                    selected_index = ffcount;
+                    found = true;
+                }
+                lstFontList.Items.Add(ff);
+                ffcount++;
+            }
+            //set default font for current text printer
+            //
+            _typefaceStore = new TypefaceStore();
+            _typefaceStore.FontCollection = installedFontCollection;
+            //set default font for current text printer
+            _currentTextPrinter.Typeface = _typefaceStore.GetTypeface(selectedFF);
+            //---------- 
+
+
+            if (selected_index < 0) { selected_index = 0; }
+            lstFontList.SelectedIndex = selected_index;
+            lstFontList.SelectedIndexChanged += (s, e) =>
+            {
+                InstalledFont ff = lstFontList.SelectedItem as InstalledFont;
+                if (ff != null)
+                {
+                    _currentTextPrinter.Typeface = _typefaceStore.GetTypeface(ff);
+                    //sample text box 
+                    UpdateRenderOutput();
+                }
+            };
+            //----------
+            lstFontSizes.Items.AddRange(
+                new object[]{
+                    8, 9,
+                    10,11,
+                    12,
+                    14,
+                    16,
+                    18,20,22,24,26,28,36,48,72,240,300,360
+                });
+            lstFontSizes.SelectedIndexChanged += (s, e) =>
+            {
+                //new font size
+                _currentTextPrinter.FontSizeInPoints = (int)lstFontSizes.SelectedItem;
+                UpdateRenderOutput();
+            };
+            lstFontSizes.SelectedIndex = 0;
+            this.Text = "Gdi+ Sample";
+            //------ 
+        }
+        void UpdateRenderOutput()
+        {
+            //render glyph with gdi path
+            if (g == null)
+            {
+                g = this.CreateGraphics();
+            }
+            if (string.IsNullOrEmpty(this.txtInputChar.Text))
+            {
+                return;
+            }
+            //-----------------------  
+            //set some props ...
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.Clear(Color.White);
+            //credit:
+            //http://stackoverflow.com/questions/1485745/flip-coordinates-when-drawing-to-control
+            g.ScaleTransform(1.0F, -1.0F);// Flip the Y-Axis 
+            g.TranslateTransform(0.0F, -(float)500);// Translate the drawing area accordingly   
+
+
+            _currentTextPrinter.FillBackground = this.chkFillBackground.Checked;
+            _currentTextPrinter.DrawOutline = this.chkBorder.Checked;
+
+            //-----------------------  
+            _currentTextPrinter.HintTechnique = (HintTechnique)lstHintList.SelectedItem;
+            _currentTextPrinter.PositionTechnique = (PositionTechnique)cmbPositionTech.SelectedItem;
+            _currentTextPrinter.TargetGraphics = g;
+            //render at specific pos
+            int lineSpacingPx = (int)System.Math.Ceiling(_currentTextPrinter.FontLineSpacingPx);
+            float x_pos = 0, y_pos = y_pos = lineSpacingPx * 2; //start 1st line
+            char[] textBuffer = txtInputChar.Text.ToCharArray();
+
+            //test draw multiple lines
+
+            for (int i = 0; i < 3; ++i)
+            {
+                _currentTextPrinter.DrawString(
+                 textBuffer,
+                 0,
+                 textBuffer.Length,
+                 x_pos,
+                 y_pos
+                );
+                //draw top to bottom 
+                y_pos -= lineSpacingPx;
+            }
+            //
+            //-----------------------  
+            //transform back
+            g.ScaleTransform(1.0F, -1.0F);// Flip the Y-Axis 
+            g.TranslateTransform(0.0F, -(float)500);// Translate the drawing area accordingly            
+
+            //-----------------------   
+        }
+
+        UnscaledGlyphPlanList _reusableUnscaledGlyphPlanList = new UnscaledGlyphPlanList();
+
+
+        void RenderAndShowMeasureBox()
+        {
+            bool flipY = chkFlipY.Checked;
+
+           
+
+            //set some Gdi+ props... 
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            g.Clear(Color.White);
+
+            Typography.OpenFont.Typeface typeface = _currentTextPrinter.Typeface;
+            Typography.OpenFont.TypefaceExtension2.UpdateAllCffGlyphBounds(typeface);
+            
+
+            float pxscale = typeface.CalculateScaleToPixelFromPointSize(_currentTextPrinter.FontSizeInPoints); 
+            int lineSpacing = (int)System.Math.Ceiling((double)typeface.CalculateLineSpacing(LineSpacingChoice.TypoMetric) * pxscale);
+            
+
+            if (flipY)
+            {
+                //credit:
+                //http://stackoverflow.com/questions/1485745/flip-coordinates-when-drawing-to-control
+                g.ScaleTransform(1.0F, -1.0F);// Flip the Y-Axis 
+                g.TranslateTransform(0.0F, -500);// Translate the drawing area accordingly   
+            }
+
+
+            //--------------------------------
+            //textspan measurement sample
+            //--------------------------------  
+            _currentTextPrinter.HintTechnique = (HintTechnique)lstHintList.SelectedItem;
+            _currentTextPrinter.PositionTechnique = (PositionTechnique)cmbPositionTech.SelectedItem;
+            _currentTextPrinter.UpdateGlyphLayoutSettings();
+
+            //render at specific pos
+            float x_pos = 0, y_pos = lineSpacing * 2; 
+
+            char[] textBuffer = txtInputChar.Text.ToCharArray();
+
+            //Example 1: this is a basic draw sample
+            _currentTextPrinter.FillColor = Color.Black;
+            _currentTextPrinter.TargetGraphics = g;
+            _currentTextPrinter.DrawString(
+                textBuffer,
+                 0,
+                 textBuffer.Length,
+                 x_pos,
+                 y_pos
+                );
+            //
+            //--------------------------------------------------
+            //Example 2: print glyph plan to 'user' list-> then draw it (or hold it/ not draw)                         
+            //you can create you own class to hold userGlyphPlans.***
+            //2.1
+            _reusableUnscaledGlyphPlanList.Clear();
+            _currentTextPrinter.GenerateGlyphPlan(textBuffer, 0, textBuffer.Length, _reusableUnscaledGlyphPlanList);
+            //2.2
+            //and we can print the formatted glyph plan later.
+            y_pos -= lineSpacing;//next line
+            _currentTextPrinter.FillColor = Color.Red;
+
+            _currentTextPrinter.DrawFromGlyphPlans(
+                  new GlyphPlanSequence(_reusableUnscaledGlyphPlanList),
+                  x_pos,
+                  y_pos
+             );
+
+            //Example 3: MeasureString    
+            UnscaledGlyphPlanList glyphPlans = new UnscaledGlyphPlanList();
+            _currentTextPrinter.GlyphLayoutMan.GenerateUnscaledGlyphPlans(glyphPlans);
+            MeasuredStringBox strBox = _currentTextPrinter.GlyphLayoutMan.LayoutAndMeasureString(
+              textBuffer, 0, textBuffer.Length,
+              _currentTextPrinter.FontSizeInPoints);
+
+            int j = glyphPlans.Count;
+            float backup_xpos = x_pos;
+            for (int i = 0; i < j; ++i)
+            {
+                UnscaledGlyphPlan glyphPlan = glyphPlans[i];
+                Typography.OpenFont.Glyph glyph = typeface.GetGlyphByIndex(glyphPlan.glyphIndex);
+                //
+                Typography.OpenFont.Bounds b = glyph.Bounds;
+                //
+                float xmin = b.XMin * pxscale;
+                float ymin = b.YMin * pxscale;
+                //
+                float xmax = b.XMax * pxscale;
+                float ymax = b.YMax * pxscale;
+                //
+                float glyph_x = x_pos + glyphPlan.OffsetX;
+                g.DrawRectangle(Pens.Red, glyph_x + xmin, y_pos + ymin, xmax - xmin, ymax - ymin);
+                x_pos += glyphPlan.AdvanceX * pxscale;
+            }
+
+            x_pos = backup_xpos;
+
+            g.FillRectangle(Brushes.Red, new RectangleF(0, 0, 5, 5));//reference point(0,0)
+            g.FillRectangle(Brushes.Green, new RectangleF(x_pos, y_pos, 3, 3));
+
+
+            float x_pos2 = x_pos + strBox.width + 10;
+
+
+            g.DrawRectangle(Pens.Black, x_pos, y_pos + strBox.DescendingInPx, strBox.width, strBox.ClipHeightInPx);
+            g.DrawRectangle(Pens.Red, x_pos, y_pos + strBox.DescendingInPx, strBox.width, strBox.LineSpaceInPx);
+
+            g.DrawLine(Pens.Blue, x_pos, y_pos, x_pos2, y_pos); //baseline
+            g.DrawLine(Pens.Green, x_pos, y_pos + strBox.DescendingInPx, x_pos2, y_pos + strBox.DescendingInPx);//descending
+            g.DrawLine(Pens.Magenta, x_pos, y_pos + strBox.AscendingInPx, x_pos2, y_pos + strBox.AscendingInPx);//ascending
+
+
+            ////------------
+            ////draw another line (for reference)
+            y_pos -= lineSpacing;//next line
+
+
+            _currentTextPrinter.FillColor = Color.Black;
+
+            _currentTextPrinter.DrawFromGlyphPlans(
+                  new GlyphPlanSequence(_reusableUnscaledGlyphPlanList),
+                  x_pos,
+                  y_pos
+             );
+            //transform back
+            if (flipY)
+            {
+                g.ScaleTransform(1.0F, -1.0F);// Flip the Y-Axis 
+                g.TranslateTransform(0.0F, -500);// Translate the drawing area accordingly   
+            }
+
+            //---------
+
+            //txtMsgInfo.Text = "choice:" + choice.ToString() + "=" + lineSpacing.ToString();
+        }
+        private void cmdMeasureTextSpan_Click(object sender, System.EventArgs e)
+        {
+            RenderAndShowMeasureBox();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, System.EventArgs e)
+        {
+            RenderAndShowMeasureBox();
+        }
+
+
+    }
+}
