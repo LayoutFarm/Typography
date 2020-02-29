4c68460f41975090a525d516af799418a54f6abf
win <wintercoredev@gmail.com>
2020-02-29 15:03:00 +0700

    973.1: minor

--- Typography.OpenFont/Tables.AdvancedLayout/ClassDefTable.cs
+++ Typography.OpenFont/Tables.AdvancedLayout/ClassDefTable.cs
@@ -163,7 +163,7 @@ namespace Typography.OpenFont.Tables
         }
 
 
-        public int GetClassValue(int glyphIndex)
+        public int GetClassValue(ushort glyphIndex)
         {
             switch (Format)
             {
