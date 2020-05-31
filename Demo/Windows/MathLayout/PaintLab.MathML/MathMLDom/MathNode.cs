95c4dc6f5d1fa539d2132ac6edbb3c95529499dc
win <wintercoredev@gmail.com>
2020-05-31 21:09:50 +0700

    1030: controller

--- Demo/Windows/MathLayout/PaintLab.MathML/MathMLDom/MathNode.cs
+++ Demo/Windows/MathLayout/PaintLab.MathML/MathMLDom/MathNode.cs
@@ -7,6 +7,14 @@ namespace MathLayout
     {
         List<MathNode> _children = new List<MathNode>();
         Dictionary<string, string> _attributes = new Dictionary<string, string>();
+
+        object _controller;
+
+        public void SetController(object controller) => _controller = controller;
+
+        public static object UnsafeGetController(MathNode elem) => elem._controller;
+
+
         public int ChildCount => _children.Count;
         public MathNode GetNode(int index) => _children[index];
         public abstract string Name { get; }
@@ -31,14 +39,19 @@ namespace MathLayout
         {
             _children.Add(node);
         }
+
+#if DEBUG
         public override string ToString()
         {
             return Name + "," + Text;
         }
+#endif
     }
     public class math : MathNode
     {
         public override string Name => "math";
         public override string ToString() => Name;
     }
+
+
 }
