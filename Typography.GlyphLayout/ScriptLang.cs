081bfdb4b0227c43c22c2d9405063e539e4611e2
win <wintercoredev@gmail.com>
2020-06-05 14:50:39 +0700

    1055: ScriptLang

---
+++ Typography.GlyphLayout/ScriptLang.cs
@@ -0,0 +1,31 @@
+﻿//MIT, 2016-present, WinterDev
+
+namespace Typography.OpenFont
+{
+    /// <summary>
+    /// script tag and syslang tag for GSUB, GPOS
+    /// </summary>
+    public sealed class ScriptLang
+    {
+        /// <summary>
+        /// script tag
+        /// </summary>
+        public readonly string scriptTag;
+        /// <summary>
+        /// syslang tag
+        /// </summary>
+        public readonly string sysLangTag;
+
+        public ScriptLang(string scriptTag, string sysLangTag = "")
+        {
+            this.scriptTag = scriptTag;
+            this.sysLangTag = sysLangTag;
+        }
+        public override string ToString()
+        {
+            return this.scriptTag + ":" + scriptTag;
+
+        }
+
+    }
+}
