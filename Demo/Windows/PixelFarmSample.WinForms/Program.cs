906bd96c9698708d1b1fda2adde6598309a314f9
win <wintercoredev@gmail.com>
2020-07-03 18:57:54 +0700

    1195: change default dpi value

--- Demo/Windows/PixelFarmSample.WinForms/Program.cs
+++ Demo/Windows/PixelFarmSample.WinForms/Program.cs
@@ -1,6 +1,8 @@
 ﻿//MIT, 2016-2017, WinterDev
 using System;
 using System.Windows.Forms;
+using Typography.OpenFont;
+
 namespace SampleWinForms
 {
     static class Program
@@ -11,10 +13,40 @@ namespace SampleWinForms
         [STAThread]
         static void Main()
         {
+            //---------
+            //user can skip this, and set it by app-manifest
+            //but here I, set DPI-aware by API :)
+            //-------
+
+
+            bool dpi_result = SetProcessDPIAware();
+            Typeface.DefaultDpi = GetDpiForSystem();
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
+
+        //-------
+        //https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-setprocessdpiaware?redirectedfrom=MSDN
+        [System.Runtime.InteropServices.DllImport("user32")]
+        static extern bool SetProcessDPIAware();
+
+        //UINT GetDpiForWindow(
+        //  HWND hwnd
+        //);
+        //https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getdpiforwindow
+
+        [System.Runtime.InteropServices.DllImport("user32")]
+        static extern uint GetDpiForWindow(IntPtr ptr);
+
+        //UINT GetDpiForSystem();
+        //https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-getdpiforsystem
+        [System.Runtime.InteropServices.DllImport("user32")]
+        static extern uint GetDpiForSystem();
+        //-------
     }
 
 }
