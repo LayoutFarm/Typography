1539cefad624479dce12f91cdfacbf0f5c8481c4
win <wintercoredev@gmail.com>
2020-07-04 07:22:46 +0700

    1197.1

--- Typography.TextBreak/Typography.TextBreak/ScriptTagsAndLanguageSystemTags/Unicode5_1Ranges.cs
+++ Typography.TextBreak/Typography.TextBreak/ScriptTagsAndLanguageSystemTags/Unicode5_1Ranges.cs
@@ -5,63 +5,6 @@ using System.Collections.Generic;
 namespace Typography.OpenFont
 {
 
-    //https://docs.microsoft.com/en-us/typography/opentype/spec/os2#ulunicoderange1-bits-031ulunicoderange2-bits-3263ulunicoderange3-bits-6495ulunicoderange4-bits-96127     
-
-    public class UnicodeRangeInfo
-    {
-        /// <summary>
-        /// begin code point
-        /// </summary>
-        public int StarCodepoint { get; }
-        /// <summary>
-        /// end codepoint
-        /// </summary>
-        public int EndCodepoint { get; }
-
-        public string Name { get; }
-
-        internal UnicodeRangeInfo(int startAt, int endAt, string name)
-        {
-            StarCodepoint = startAt;
-            EndCodepoint = endAt;
-            Name = name;
-        }
-        public bool IsInRange(int codepoint) => codepoint >= StarCodepoint && codepoint <= EndCodepoint;
-        public override string ToString() => Name;
-    }
-
-    public readonly struct BitposAndAssciatedUnicodeRanges
-    {
-        public readonly int Bitpos;
-        public readonly UnicodeRangeInfo[] Ranges;
-        public BitposAndAssciatedUnicodeRanges(int bitpos, UnicodeRangeInfo[] ranges)
-        {
-            Bitpos = bitpos;
-            Ranges = ranges;
-        }
-        public override string ToString()
-        {
-            System.Text.StringBuilder sb = new System.Text.StringBuilder();
-            sb.Append(Bitpos + ",");
-            for (int i = 0; i < Ranges.Length; ++i)
-            {
-                sb.Append(Ranges[i].ToString());
-            }
-            return sb.ToString();
-        }
-        public bool IsInRange(int codepoint)
-        {
-            for (int i = 0; i < Ranges.Length; ++i)
-            {
-                if (Ranges[i].IsInRange(codepoint))
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-    }
-
 
     public static class Unicode5_1Ranges
     {
@@ -585,6 +528,4 @@ _(127,Reserved127)
 
         public const int MAX_BITPOS = 122;
     }
-
-
 }
