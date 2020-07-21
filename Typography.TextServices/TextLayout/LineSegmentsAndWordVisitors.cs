30cf8335d6d98ce982645f30a433341d691df289
win <wintercoredev@gmail.com>
2020-07-21 09:34:09 +0700

    593: move code TextSpanMeasureResult

--- Typography.TextServices/TextLayout/LineSegmentsAndWordVisitors.cs
+++ Typography.TextServices/TextLayout/LineSegmentsAndWordVisitors.cs
@@ -92,5 +92,14 @@ namespace Typography.TextLayout
         }
     }
 
+    public struct TextSpanMeasureResult
+    {
+        public int[] outputXAdvances;
+        public int outputTotalW;
+        public ushort lineHeight;
 
+        public bool hasSomeExtraOffsetY;
+        public short minOffsetY;
+        public short maxOffsetY;
+    }
 }
