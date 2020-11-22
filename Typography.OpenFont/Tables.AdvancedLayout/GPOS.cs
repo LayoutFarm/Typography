7fa2c452cf1832a18b9508b17389ddc4799805b9
Sam Hocevar <sam@hocevar.net>
2020-11-22 15:19:06 +0100

    Implement GPOS lookup subtables type 8, format 3.

--- Typography.OpenFont/Tables.AdvancedLayout/GPOS.cs
+++ Typography.OpenFont/Tables.AdvancedLayout/GPOS.cs
@@ -1103,7 +1103,46 @@ namespace Typography.OpenFont.Tables
 
                 public override void DoGlyphPosition(IGlyphPositions inputGlyphs, int startAt, int len)
                 {
-                    Utils.WarnUnimplemented("GPOS Lookup Sub Table Type 8 Format 3");
+                    startAt = Math.Max(startAt, BacktrackCoverages.Length);
+                    int lim = Math.Min(startAt + len, inputGlyphs.Count) - (InputGlyphCoverages.Length - 1) - LookaheadCoverages.Length;
+                    for (int pos = startAt; pos < lim; ++pos)
+                    {
+                        DoGlyphPositionAt(inputGlyphs, pos);
+                    }
+                }
+
+                protected void DoGlyphPositionAt(IGlyphPositions inputGlyphs, int pos)
+                {
+                    // Check all coverages: if any of them does not match, abort substitution
+                    for (int i = 0; i < InputGlyphCoverages.Length; ++i)
+                    {
+                        if (InputGlyphCoverages[i].FindPosition(inputGlyphs.GetGlyph(pos + i, out var unused)) < 0)
+                        {
+                            return;
+                        }
+                    }
+
+                    for (int i = 0; i < BacktrackCoverages.Length; ++i)
+                    {
+                        if (BacktrackCoverages[i].FindPosition(inputGlyphs.GetGlyph(pos - 1 - i, out var unused)) < 0)
+                        {
+                            return;
+                        }
+                    }
+
+                    for (int i = 0; i < LookaheadCoverages.Length; ++i)
+                    {
+                        if (LookaheadCoverages[i].FindPosition(inputGlyphs.GetGlyph(pos + InputGlyphCoverages.Length + i, out var unused)) < 0)
+                        {
+                            return;
+                        }
+                    }
+
+                    foreach (var plr in PosLookupRecords)
+                    {
+                        var lookup = OwnerGPos.LookupList[plr.lookupListIndex];
+                        lookup.DoGlyphPosition(inputGlyphs, pos + plr.seqIndex, InputGlyphCoverages.Length - plr.seqIndex);
+                    }
                 }
             }
 
